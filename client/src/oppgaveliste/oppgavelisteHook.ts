3371533f8e558a8044cc67cd4207cdb1cb93af77
matsbyfl <mats@byfuglien.net>
2021-07-14 10:23:24 +0200

    debug

--- client/src/oppgaveliste/oppgavelisteHook.ts
+++ client/src/oppgaveliste/oppgavelisteHook.ts
@@ -5,6 +5,7 @@ import { httpGet } from '../io/http'
 import { Oppgave } from '../types/types.internal'
 
 export function useOppgaveliste() {
+    console.log('kaller oppgaveliste ' + 'api/oppgaver')
   const { data, error } = useSwr('api/oppgaver', httpGet)
 
   return {

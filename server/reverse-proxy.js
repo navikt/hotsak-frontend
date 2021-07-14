d0d6d01adc91c1b4c6728b52313088253c75936b
matsbyfl <mats@byfuglien.net>
2021-07-14 11:44:05 +0200

    legger til options

--- server/reverse-proxy.js
+++ server/reverse-proxy.js
@@ -5,10 +5,22 @@ const envProperties = {
   API_URL: process.env.API_URL || `http://localhost:${config.server.port}`,
 }
 
+const options = () => ({
+  parseReqBody: false,
+  proxyReqPathResolver: (req) => {
+    return pathRewriteBasedOnEnvironment(req)
+  },
+})
+
+const pathRewriteBasedOnEnvironment = (req) => {
+  console.log('Rewrite ' + req.originalUrl)
+  return req.originalUrl
+}
+
 const setupProxy = (server) => {
   console.log('setup proxy ' + envProperties.API_URL)
 
-  server.use(`/api/`, proxy(envProperties.API_URL + '/api'))
+  server.use(`/api/`, proxy(envProperties.API_URL + '/api', options()))
 }
 
 export default setupProxy

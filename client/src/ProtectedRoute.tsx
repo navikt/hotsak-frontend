d6f59a2b1d36aa0c1578fcb1eff4b81c7852283a
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2021-08-06 11:57:58 +0200

    Fikser "Ikke innlogget" redirect

--- client/src/ProtectedRoute.tsx
+++ client/src/ProtectedRoute.tsx
@@ -1,13 +1,12 @@
 import * as React from 'react'
 import { Redirect, Route, RouteProps } from 'react-router'
 
-//import { useRecoilValue } from 'recoil';
+import { useRecoilValue } from 'recoil';
 
-//import { authState } from '../state/authentication';
+import { authState } from './state/authentication';
 
 export const ProtectedRoute = ({ children, ...rest }: RouteProps) => {
-  //const { isLoggedIn } = useRecoilValue(authState);
-  var isLoggedIn = true
+  const { isLoggedIn } = useRecoilValue(authState);
 
   return <Route {...rest} render={() => (isLoggedIn !== false ? <>{children}</> : <Redirect to="/uautorisert" />)} />
 }

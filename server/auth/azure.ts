ef20838a25c9bef75501856e6572622924de0a4d
matsbyfl <mats@byfuglien.net>
2021-08-25 10:42:29 +0200

    Skippr oppsett av azure klient lokal og i labs

--- server/auth/azure.ts
+++ server/auth/azure.ts
@@ -1,48 +1,49 @@
-import { Client, custom, Issuer } from 'openid-client';
+import { Client, custom, Issuer } from 'openid-client'
 
-import logger from '../logging';
-import { OidcConfig } from '../types';
-import { setup as proxy } from './proxy';
+import logger from '../logging'
+import { OidcConfig } from '../types'
+import { setup as proxy } from './proxy'
 
-('use strict');
+;('use strict')
 
-let azureClient;
-const proxyAgent = proxy(Issuer, custom);
+let azureClient
+const proxyAgent = proxy(Issuer, custom)
 
 const setup = (config: OidcConfig) => {
-    return new Promise<void | Client>((resolve, reject) => {
-        if (process.env.NODE_ENV === 'development' || process.env.NAIS_CLUSTER_NAME === 'labs-gcp') {
-            resolve();
-        }
+  return new Promise<void | Client>((resolve, reject) => {
+    console.log('PROcess env cluster name', process.env.NAIS_CLUSTER_NAME)
+    if (process.env.NODE_ENV === 'development' || process.env.NAIS_CLUSTER_NAME === 'labs-gcp') {
+      resolve()
+    } else {
+      Issuer.discover(config.wellKnownEndpoint)
+        .then((azure) => {
+          logger.info(`Discovered issuer ${azure.issuer}`)
+          azureClient = new azure.Client({
+            client_id: config.clientID,
+            client_secret: config.clientSecret,
+            redirect_uris: [],
+            response_types: ['code'],
+          })
 
-        Issuer.discover(config.wellKnownEndpoint)
-            .then((azure) => {
-                logger.info(`Discovered issuer ${azure.issuer}`);
-                azureClient = new azure.Client({
-                    client_id: config.clientID,
-                    client_secret: config.clientSecret,
-                    redirect_uris: [],
-                    response_types: ['code'],
-                });
+          if (proxyAgent) {
+            azure[custom.http_options] = function (options) {
+              options.agent = proxyAgent
+              return options
+            }
+            azureClient[custom.http_options] = function (options) {
+              options.agent = proxyAgent
+              return options
+            }
+          }
 
-                if (proxyAgent) {
-                    azure[custom.http_options] = function (options) {
-                        options.agent = proxyAgent;
-                        return options;
-                    };
-                    azureClient[custom.http_options] = function (options) {
-                        options.agent = proxyAgent;
-                        return options;
-                    };
-                }
+          resolve(azureClient)
+        })
+        .catch((err) => {
+          console.log('Klarte ikke sette opp azure client')
+          reject(err)
+        })
+    }
+  })
+}
 
-                resolve(azureClient);
-            })
-            .catch((err) => {
-              console.log("Klarte ikke sette opp azure client")
-                reject(err);
-            });
-    });
-};
-
-export default { setup };
+export default { setup }

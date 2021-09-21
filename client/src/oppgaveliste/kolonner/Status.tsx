016f28b82a6395fda9eea4aa269d4c76134d182a
matsbyfl <mats@byfuglien.net>
2021-09-21 16:24:59 +0200

    Litt bedre plass til status kolonna i oppgavelista

--- client/src/oppgaveliste/kolonner/Status.tsx
+++ client/src/oppgaveliste/kolonner/Status.tsx
@@ -15,7 +15,7 @@ export const Status = React.memo(({ status, saksID }: StatusProps) => {
     const id = `status-${saksID}`;
    // const formatertStatus = capitalize(status)
   return (
-    <CellContent width={120}  data-for={id} data-tip={status}>
+    <CellContent width={150}  data-for={id} data-tip={status}>
       <TekstMedEllipsis>{status}</TekstMedEllipsis>
       {status.length > 18 && <Tooltip id={id} />}
     </CellContent>

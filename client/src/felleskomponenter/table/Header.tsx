a35b5e8995d76a04deb7e3afcbdb5013aa6c6b5c
matsbyfl <mats@byfuglien.net>
2021-09-21 16:23:58 +0200

    Bold tekst i table header i oppgaveliste

--- client/src/felleskomponenter/table/Header.tsx
+++ client/src/felleskomponenter/table/Header.tsx
@@ -1,8 +1,9 @@
 import styled from 'styled-components/macro'
 
 export const Header = styled.th`
-  padding: 1rem 1rem 1.5rem 1rem;
+  padding: 1rem 1rem 1rem 0.5rem;
   border-bottom: 1px solid var(--navds-color-border);
   text-align: left;
   white-space: nowrap;
+  font-weight: bold;
 `

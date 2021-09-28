05444a11b92676be6f71309d48e56aa43c1105f7
Håkon <hakonhagen2@gmail.com>
2021-09-28 10:29:59 +0200

    invalid js

--- server/envSettings.ts
+++ server/envSettings.ts
@@ -4,8 +4,8 @@ const createEnvSettingsFile = (path: string)  => {
   fs.writeFileSync(path, `
     window.appSettings = {
       MILJO: '${process.env.NAIS_CLUSTER_NAME}',
-      USE_MSW: ${process.env.USE_MSW}
-      foobar: true,
+      USE_MSW: ${process.env.USE_MSW},
+      foobar: true
     }
   `
   )

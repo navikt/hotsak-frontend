943c605869496a563e220ecc9a280f5b15577990
matsbyfl <mats@byfuglien.net>
2021-10-04 16:23:19 +0200

    Legger til nytt favikon

--- client/src/utils/cookie.ts
+++ client/src/utils/cookie.ts
@@ -13,8 +13,6 @@ export enum CookieKey {
     Groups = 'groups',
 }
 
-console.log("NAIS_CLUSTER_NAME ", process.env.NAIS_CLUSTER_NAME)
-
 if (process.env.NODE_ENV === 'development' || process.env.NAIS_CLUSTER_NAME === 'labs-gcp') {
     document.cookie = `speil=dev-cookie.${btoa(
         JSON.stringify({

8d830c9e584100a6e96c1e8a05478c62bd597d43
matsbyfl <mats@byfuglien.net>
2021-12-21 18:57:41 +0100

    Fjerner unødvendig padding i tabell header

--- client/src/felleskomponenter/table/Header.tsx
+++ client/src/felleskomponenter/table/Header.tsx
@@ -1,7 +1,6 @@
 import styled from 'styled-components/macro'
 
 export const Header = styled.th`
-  padding: 1rem 1rem 1rem 0.5rem;
   border-bottom: 1px solid var(--navds-semantic-color-border-muted);
   text-align: left;
   white-space: nowrap;

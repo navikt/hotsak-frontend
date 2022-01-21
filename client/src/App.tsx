3b62b95ce96bf573a2a016d667b898984a988fd7
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2022-01-21 12:41:27 +0100

    Sender event med oppløsing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,21 @@
 import React from 'react'
-import { ErrorBoundary } from 'react-error-boundary'
-import { hot } from 'react-hot-loader'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import { RecoilRoot } from 'recoil'
+import {ErrorBoundary} from 'react-error-boundary'
+import {hot} from 'react-hot-loader'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import {RecoilRoot} from 'recoil'
 import 'reset-css'
 
-import { IkkeLoggetInn } from './routes/IkkeLoggetInn'
+import {IkkeLoggetInn} from './routes/IkkeLoggetInn'
 
 import './App.less'
 //import ReactModal from 'react-modal';
-import { Toppmeny } from './Header'
-import { ProtectedRoute } from './ProtectedRoute'
-import { GlobalFeilside } from './feilsider/GlobalFeilside'
-import { PageNotFound } from './feilsider/PageNotFound'
-import { Routes } from './routes'
-import { useAuthentication } from './state/authentication'
+import {Toppmeny} from './Header'
+import {ProtectedRoute} from './ProtectedRoute'
+import {GlobalFeilside} from './feilsider/GlobalFeilside'
+import {PageNotFound} from './feilsider/PageNotFound'
+import {Routes} from './routes'
+import {useAuthentication} from './state/authentication'
+import {amplitude_taxonomy, logAmplitudeEvent} from './utils/amplitude'
 
 const Oppgaveliste = React.lazy(() => import('./oppgaveliste/Oppgaveliste'))
 const Saksbilde = React.lazy(() => import('./saksbilde/Saksbilde'))
@@ -24,7 +25,7 @@ const Saksbilde = React.lazy(() => import('./saksbilde/Saksbilde'))
 
 function App() {
   useAuthentication()
-
+  logUserStats()
   return (
     <ErrorBoundary FallbackComponent={GlobalFeilside}>
       <Toppmeny />
@@ -40,7 +41,7 @@ function App() {
           <ProtectedRoute path={Routes.Saksbilde}>
             <Saksbilde />
           </ProtectedRoute>
-          <Route path="*">
+          <Route path='*'>
             <PageNotFound />
           </Route>
         </Switch>
@@ -50,6 +51,12 @@ function App() {
     </ErrorBoundary>
   )
 }
+
+const logUserStats = () => {
+  const {innerWidth: width, innerHeight: height} = window;
+  logAmplitudeEvent(amplitude_taxonomy.CLIENT_RESOLUTION, {width, height})
+}
+
 
 const withRoutingAndState = (Component: React.ComponentType) => () => (
   <BrowserRouter>

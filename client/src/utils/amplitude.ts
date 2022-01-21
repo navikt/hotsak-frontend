c66b5bcf57e2e6ddfafe8a5ebaabca37b974af9b
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2022-01-21 13:11:31 +0100

    samler res i felt som kan grupperes på

--- client/src/utils/amplitude.ts
+++ client/src/utils/amplitude.ts
@@ -7,7 +7,7 @@ export enum amplitude_taxonomy {
   SOKNAD_OVERFORT_TIL_GOSYS = 'overført søknad til Gosys',
   SAK_OVERTATT = 'sak overtatt av ny saksbehandler',
   NAVIGASJON_ETTER_HENDELSE = 'neste navigasjon foretatt etter hendelse',
-  CLIENT_RESOLUTION = 'client-resolution'
+  CLIENT_INFO = 'client info'
 }
 
 export const initAmplitude = () => {

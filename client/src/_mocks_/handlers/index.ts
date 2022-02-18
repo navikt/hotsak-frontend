13c9fa8164bfb2e6e50c591a50e54cd20dbae4c4
matsbyfl <mats@byfuglien.net>
2022-02-18 14:04:39 +0100

    Legger til mock av hjelpemiddeloversikt

--- client/src/_mocks_/handlers/index.ts
+++ client/src/_mocks_/handlers/index.ts
@@ -1,6 +1,7 @@
 import grunndataHandlers from './grunndata'
 import saksbehandlingHandlers from './saksbehandling'
+import hjelpemiddeloversiktHandlers from './hjelpemiddeloversikt'
 
-const handlers = [...saksbehandlingHandlers, ...grunndataHandlers]
+const handlers = [...saksbehandlingHandlers, ...grunndataHandlers, ...hjelpemiddeloversiktHandlers]
 
 export default handlers

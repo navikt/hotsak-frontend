70b0e1fa8406149177e5fd1b84ce5455d0bdc808
Erik Eiesland <erik@eies.land>
2022-04-08 10:51:09 +0200

    fjerner noen flere trailing-slash i msw-handlers

--- client/src/_mocks_/handlers/hjelpemiddeloversikt.ts
+++ client/src/_mocks_/handlers/hjelpemiddeloversikt.ts
@@ -3,28 +3,26 @@ import { rest } from 'msw'
 import hjelpemiddeloversikt from '../mockdata/hjelpemiddeloversikt.json'
 
 const hjelpemiddeloversiktHandlers = [
-    rest.post(`/api/hjelpemiddeloversikt/`, (req, res, ctx) => {
-        // @ts-ignore
-        const brukersFodselsnummer = req.body.brukersFodselsnummer
+  rest.post(`/api/hjelpemiddeloversikt`, (req, res, ctx) => {
+    // @ts-ignore
+    const brukersFodselsnummer = req.body.brukersFodselsnummer
 
-         if(brukersFodselsnummer === '06115559891') {
-            return res(ctx.status(200), ctx.json([]))
-         }
+    if (brukersFodselsnummer === '06115559891') {
+      return res(ctx.status(200), ctx.json([]))
+    }
 
-         if(brukersFodselsnummer === '19044238651') { // Petter Andreas
-            return res(ctx.status(200), ctx.json(hjelpemiddeloversikt[0]))
-         }
-         else if(brukersFodselsnummer === '13044238651') { // Mia Cathrine
-            return res(ctx.status(200), ctx.json(hjelpemiddeloversikt[1]))
-         }
-         else if(brukersFodselsnummer === '500') {
-            return res(ctx.status(500), ctx.text("Å nei og nei. Dette kunne virkelig ikke gått verre"))
-         }
-         else {
-            return res(ctx.status(200), ctx.json(hjelpemiddeloversikt[2]))
-         }
-        
-    })
+    if (brukersFodselsnummer === '19044238651') {
+      // Petter Andreas
+      return res(ctx.status(200), ctx.json(hjelpemiddeloversikt[0]))
+    } else if (brukersFodselsnummer === '13044238651') {
+      // Mia Cathrine
+      return res(ctx.status(200), ctx.json(hjelpemiddeloversikt[1]))
+    } else if (brukersFodselsnummer === '500') {
+      return res(ctx.status(500), ctx.text('Å nei og nei. Dette kunne virkelig ikke gått verre'))
+    } else {
+      return res(ctx.status(200), ctx.json(hjelpemiddeloversikt[2]))
+    }
+  }),
 ]
 
 export default hjelpemiddeloversiktHandlers
--- client/src/_mocks_/handlers/saksoversikt.ts
+++ client/src/_mocks_/handlers/saksoversikt.ts
@@ -1,28 +1,26 @@
 import { rest } from 'msw'
+
 import saksoversikt from '../mockdata/saksoversikt.json'
 
-
 const saksoversiktHandlers = [
-    rest.post(`/api/saksoversikt/`, (req, res, ctx) => {
-         // @ts-ignore
-         const brukersFodselsnummer = req.body.brukersFodselsnummer
+  rest.post(`/api/saksoversikt`, (req, res, ctx) => {
+    // @ts-ignore
+    const brukersFodselsnummer = req.body.brukersFodselsnummer
 
-        if(brukersFodselsnummer === '19044238651') { // Petter Andreas
-            return res(ctx.status(200), ctx.json(saksoversikt[0]))
-         }
-         else if(brukersFodselsnummer === '13044238651') { // Mia Cathrine
-            return res(ctx.status(200), ctx.json(saksoversikt[1]))
-         }
-         else if(brukersFodselsnummer === '1234') {
-            return res(ctx.status(500))
-         }
-         else if( brukersFodselsnummer === "6666") {
-             return res(ctx.status(200), ctx.json({hotsakSaker: []}))
-         }
-         else {
-            return res(ctx.status(200), ctx.json(saksoversikt[2]))
-         }
-    })
+    if (brukersFodselsnummer === '19044238651') {
+      // Petter Andreas
+      return res(ctx.status(200), ctx.json(saksoversikt[0]))
+    } else if (brukersFodselsnummer === '13044238651') {
+      // Mia Cathrine
+      return res(ctx.status(200), ctx.json(saksoversikt[1]))
+    } else if (brukersFodselsnummer === '1234') {
+      return res(ctx.status(500))
+    } else if (brukersFodselsnummer === '6666') {
+      return res(ctx.status(200), ctx.json({ hotsakSaker: [] }))
+    } else {
+      return res(ctx.status(200), ctx.json(saksoversikt[2]))
+    }
+  }),
 ]
 
 export default saksoversiktHandlers

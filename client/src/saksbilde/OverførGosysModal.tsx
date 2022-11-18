402eb64c5f8f21a0178005d7ead8986dfab57b50
erlingjd <16976922+erlingjd@users.noreply.github.com>
2022-11-18 10:39:55 +0100

    Endrer tekster og alternativer for overføring til Gosys + sjekker at det oppgis begrunnelse hvis årsak 'Annet'

--- client/src/saksbilde/OverførGosysModal.tsx
+++ client/src/saksbilde/OverførGosysModal.tsx
@@ -21,21 +21,23 @@ export const OverførGosysModal: React.FC<OverførGosysModalProps> = ({ open, on
   const [valgteArsaker, setValgteArsaker] = useState<string[]>([])
   const [begrunnelse, setBegrunnelse] = useState<string>('')
   const [error, setError] = useState('')
+  const manglerArsak = !valgteArsaker.length
+  const manglerBegrunnelse = valgteArsaker.includes('Annet') && begrunnelse.trim().length < 3
 
   return (
     <DialogBoks shouldCloseOnOverlayClick={false} open={open} onClose={onClose}>
       <DialogBoks.Content>
         <Heading level="1" size="medium" spacing>
-          Vil du overføre saken til Gosys?
+          Overfør til Gosys
         </Heading>
         <Tekst>
-          Hvis saken overføres til Gosys, vil den dukke opp som en vanlig journalføringsoppgave. Journalføring og videre
-          saksbehandling må gjøres manuelt i Gosys og Infotrygd. Merk at det kan ta noen minutter før saken dukker opp i
-          Gosys.
+          Hvis du overfører saken til Gosys, vil den dukke opp som en vanlig journalføringsoppgave. Journalføring og
+          videre saksbehandling må gjøres manuelt i Gosys og Infotrygd. Merk at det kan ta noen minutter før saken
+          dukker opp i Gosys.
         </Tekst>
         <OverforGosysArsakCheckboxGroup
-          legend="Velg årsak til at saken må overføres til Gosys"
-          error={!valgteArsaker.length && error}
+          legend="Hva må til for at du skulle kunne behandlet denne saken i Hotsak?"
+          error={(manglerArsak || manglerBegrunnelse) && error}
           value={valgteArsaker}
           onChange={setValgteArsaker}
         >
@@ -57,20 +59,22 @@ export const OverførGosysModal: React.FC<OverførGosysModalProps> = ({ open, on
             variant="primary"
             size="small"
             onClick={() => {
-              if (valgteArsaker.length) {
+              if (manglerArsak) {
+                setError('Du må velge minst en årsak i listen over.')
+              } else if (manglerBegrunnelse) {
+                setError('Du må gi en begrunnelse når det er huket av for Annet.')
+              } else {
                 onBekreft({
                   valgteArsaker,
                   begrunnelse,
                 })
-              } else {
-                setError('Du må velge minst en årsak i listen over.')
               }
             }}
             data-cy="btn-overfor-soknad"
             disabled={loading}
             loading={loading}
           >
-            Overfør saken
+            Overfør til Gosys
           </Button>
           <Button variant="secondary" size="small" onClick={onClose} disabled={loading}>
             Avbryt
@@ -82,12 +86,9 @@ export const OverførGosysModal: React.FC<OverførGosysModalProps> = ({ open, on
 }
 
 const overforGosysArsaker: ReadonlyArray<string> = [
-  'Det må etterspørres eller legges til flere opplysninger i saken',
-  'Saken kan ikke innvilges (avslag, delvis innvilgelse eller henleggelse)',
-  'Saken skal ses på av en annen saksbehandler eller enhet',
-  'Formidler har ikke fullført nødvendig godkjenningskurs',
-  'Bruker har hjelpemiddelet fra før',
-  'Saken skal innvilges med vedtaksbrev',
+  'Mulighet for å legge inn mer informasjon i saken',
+  'Mulighet for å gjøre skriftlige vedtak i Hotsak',
+  'Personen som skal jobbe videre med saken jobber ikke i Hotsak i dag',
   'Annet',
 ]
 

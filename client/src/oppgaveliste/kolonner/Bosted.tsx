5af64006a93b3601dc9959c65769a684eeb5d425
matsbyfl <mats@byfuglien.net>
2023-03-24 08:55:43 +0100

    Håndterer at bosted kan være null

--- client/src/oppgaveliste/kolonner/Bosted.tsx
+++ client/src/oppgaveliste/kolonner/Bosted.tsx
@@ -4,7 +4,7 @@ import { TekstMedEllipsis } from '../../felleskomponenter/TekstMedEllipsis'
 import { Tooltip } from '../../felleskomponenter/Tooltip'
 
 interface BostedProps {
-  bosted: string
+  bosted?: string
   saksID: string
 }
 
@@ -12,8 +12,8 @@ export const Bosted = React.memo(({ bosted, saksID }: BostedProps) => {
   const id = `bosted-${saksID}`
   return (
     <div data-for={id} data-tip={bosted}>
-      <TekstMedEllipsis>{bosted}</TekstMedEllipsis>
-      {bosted.length > 18 && <Tooltip id={id} />}
+      <TekstMedEllipsis>{bosted ? bosted : '-'}</TekstMedEllipsis>
+      {bosted && bosted.length > 18 && <Tooltip id={id} />}
     </div>
   )
 })

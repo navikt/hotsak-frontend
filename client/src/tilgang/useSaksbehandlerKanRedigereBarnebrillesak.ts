f36b58552d15ddfd1e5705eb5dae86cb0d1ad95c
matsbyfl <mats@byfuglien.net>
2023-07-18 15:25:07 +0200

    Sak kan fortsat redigeres selv om status er AVVENTER_DOKUMENTASJON

--- client/src/tilgang/useSaksbehandlerKanRedigereBarnebrillesak.ts
+++ client/src/tilgang/useSaksbehandlerKanRedigereBarnebrillesak.ts
@@ -1,13 +1,12 @@
 import { useDebugValue } from 'react'
 
-import { Barnebrillesak, OppgaveStatusType, StegType } from '../types/types.internal'
+import { Barnebrillesak, StegType } from '../types/types.internal'
 import { useSaksbehandlerErTildeltSak } from './useSaksbehandlerErTildeltSak'
 
 export function useSaksbehandlerKanRedigereBarnebrillesak(sak?: Barnebrillesak): boolean {
   const saksbehandlerErTildeltSak = useSaksbehandlerErTildeltSak(sak)
-  const sakIVentestatus = sak?.status === OppgaveStatusType.AVVENTER_DOKUMENTASJON
   const saksbehandlerKanRedigereBarnebrillesak =
     saksbehandlerErTildeltSak && sak?.steg !== StegType.GODKJENNE && sak?.steg !== StegType.FERDIG_BEHANDLET
   useDebugValue(saksbehandlerKanRedigereBarnebrillesak)
-  return saksbehandlerKanRedigereBarnebrillesak && !sakIVentestatus
+  return saksbehandlerKanRedigereBarnebrillesak
 }

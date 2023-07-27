0aa373a126fb4b4a113a6750d63d4b395f990c6b
matsbyfl <mats@byfuglien.net>
2023-07-27 13:09:40 +0200

    Flytter statstag til egen komponent

--- client/src/saksbilde/barnebriller/Barnebrillebilde.tsx
+++ client/src/saksbilde/barnebriller/Barnebrillebilde.tsx
@@ -3,20 +3,15 @@ import { ErrorBoundary, useErrorBoundary } from 'react-error-boundary'
 import styled from 'styled-components'
 
 import { ChevronDownIcon } from '@navikt/aksel-icons'
-import { Tabs, Tag } from '@navikt/ds-react'
+import { Tabs } from '@navikt/ds-react'
 
 import { MenyKnapp } from '../../oppgaveliste/kolonner/MenyKnapp'
 
 import { brilleSidebarBredde } from '../../GlobalStyles'
 import { AlertError } from '../../feilsider/AlertError'
-import {
-  OppgaveStatusLabel,
-  OppgaveStatusType,
-  Oppgavetype,
-  StegType,
-  VedtakStatusType,
-} from '../../types/types.internal'
+import { Oppgavetype, StegType } from '../../types/types.internal'
 import { LasterPersonlinje } from '../Personlinje'
+import { StatusTag } from '../komponenter/StatusTag'
 import { useBrillesak } from '../sakHook'
 import { BarnebrilleSidebar } from './BarnebrilleSidebar'
 import { ManuellSaksbehandlingProvider, useManuellSaksbehandlingContext } from './ManuellSaksbehandlingTabContext'
@@ -70,11 +65,7 @@ const BarnebrilleContent: React.FC = React.memo(() => {
           <Tabs.Tab value={StegType.FATTE_VEDTAK.toString()} label="3. Vedtak" />
         </Tabs.List>
         <Border>
-          <TagWrapper>
-            <Tag size="small" variant={tagVariant(sak.data.status, sak.data?.vedtak?.status)}>
-              {OppgaveStatusLabel.get(sak.data.status)}
-            </Tag>
-          </TagWrapper>
+          <StatusTag sakStatus={sak.data.status} vedtakStatus={sak.data.vedtak?.status} />
           <MenyKnapp
             sakID={sak.data.sakId}
             tildeletSaksbehander={sak.data.saksbehandler}
@@ -118,27 +109,6 @@ export const BarnebrilleBilde = () => (
     </React.Suspense>
   </ErrorBoundary>
 )
-
-const tagVariant = (status: OppgaveStatusType, vedtakStatus?: VedtakStatusType) => {
-  switch (status) {
-    case OppgaveStatusType.AVVENTER_DOKUMENTASJON:
-      return 'warning'
-    case OppgaveStatusType.VEDTAK_FATTET:
-      if (vedtakStatus && vedtakStatus === VedtakStatusType.INNVILGET) return 'success'
-      if (vedtakStatus && vedtakStatus === VedtakStatusType.AVSLÅTT) return 'error'
-      else return 'info'
-    case OppgaveStatusType.AVVIST:
-    case OppgaveStatusType.RETURNERT:
-      return 'error'
-    default:
-      return 'info'
-  }
-}
-
-const TagWrapper = styled.div`
-  white-space: nowrap;
-  margin: auto;
-`
 
 const FlexWrapper = styled.div`
   display: flex;
---
+++ client/src/saksbilde/komponenter/StatusTag.tsx
@@ -0,0 +1,43 @@
+import styled from 'styled-components'
+
+import { Tag } from '@navikt/ds-react'
+
+import { OppgaveStatusLabel, OppgaveStatusType, VedtakStatusType } from '../../types/types.internal'
+
+export const StatusTag = ({
+  sakStatus,
+  vedtakStatus,
+}: {
+  sakStatus: OppgaveStatusType
+  vedtakStatus?: VedtakStatusType
+}) => {
+  return (
+    <TagWrapper>
+      <Tag size="small" variant={tagVariant(sakStatus, vedtakStatus)}>
+        {OppgaveStatusLabel.get(sakStatus)}
+      </Tag>
+    </TagWrapper>
+  )
+}
+
+const tagVariant = (status: OppgaveStatusType, vedtakStatus?: VedtakStatusType) => {
+  switch (status) {
+    case OppgaveStatusType.AVVENTER_DOKUMENTASJON:
+      return 'warning'
+    case OppgaveStatusType.VEDTAK_FATTET:
+      if (vedtakStatus && vedtakStatus === VedtakStatusType.INNVILGET) return 'success'
+      if (vedtakStatus && vedtakStatus === VedtakStatusType.AVSLÅTT) return 'error'
+      else return 'info'
+    case OppgaveStatusType.AVVIST:
+    case OppgaveStatusType.RETURNERT:
+      return 'error'
+    default:
+      return 'info'
+  }
+}
+
+const TagWrapper = styled.div`
+  white-space: nowrap;
+  margin: auto;
+  padding-right: var(--a-spacing-4);
+`

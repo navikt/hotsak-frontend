d03c3f75beea3b3ab5568b7e228f3f427a2d61e5
matsbyfl <mats@byfuglien.net>
2023-09-21 22:31:14 +0200

    Oppdaterer format i lesevisningen også

--- client/src/saksbilde/barnebriller/steg/søknadsregistrering/RegistrerSøknadLesevisning.tsx
+++ client/src/saksbilde/barnebriller/steg/søknadsregistrering/RegistrerSøknadLesevisning.tsx
@@ -62,11 +62,11 @@ export const RegistrerSøknadLesevisning: React.FC = () => {
         <Brødtekst>{capitalize(vilkårsgrunnlag?.målform)}</Brødtekst>
         <Avstand paddingTop={4}>
           <Etikett>Bestillingsdato</Etikett>
-          <Brødtekst>{formaterDato(vilkårsgrunnlag?.bestillingsdato.toString())}</Brødtekst>
+          <Brødtekst>{formaterDato(vilkårsgrunnlag?.data?.bestillingsdato.toString())}</Brødtekst>
         </Avstand>
         <Avstand paddingTop={4}>
           <Etikett>Pris på brillen</Etikett>
-          <Brødtekst>{vilkårsgrunnlag?.brillepris}</Brødtekst>
+          <Brødtekst>{vilkårsgrunnlag?.data?.brillepris}</Brødtekst>
           <Detail>
             Skal bare inkludere glass, slip av glass og innfatning, inkl moms, og brilletilpasning. Eventuell
             synsundersøkelse skal ikke inkluderes i prisen.
@@ -89,12 +89,12 @@ export const RegistrerSøknadLesevisning: React.FC = () => {
           <Rad>
             <Kolonne width="150px">
               <Brødtekst>
-                <FormatertStyrke verdi={vilkårsgrunnlag?.brilleseddel.høyreSfære} />
+                <FormatertStyrke verdi={vilkårsgrunnlag?.data?.brilleseddel.høyreSfære} />
               </Brødtekst>
             </Kolonne>
             <Kolonne width="150px">
               <Brødtekst>
-                <FormatertStyrke verdi={vilkårsgrunnlag?.brilleseddel.høyreSylinder} />
+                <FormatertStyrke verdi={vilkårsgrunnlag?.data?.brilleseddel.høyreSylinder} />
               </Brødtekst>
             </Kolonne>
           </Rad>
@@ -111,12 +111,12 @@ export const RegistrerSøknadLesevisning: React.FC = () => {
           <Rad>
             <Kolonne width="150px">
               <Brødtekst>
-                <FormatertStyrke verdi={vilkårsgrunnlag?.brilleseddel.venstreSfære} />
+                <FormatertStyrke verdi={vilkårsgrunnlag?.data?.brilleseddel.venstreSfære} />
               </Brødtekst>
             </Kolonne>
             <Kolonne width="150px">
               <Brødtekst>
-                <FormatertStyrke verdi={vilkårsgrunnlag?.brilleseddel.venstreSylinder} />
+                <FormatertStyrke verdi={vilkårsgrunnlag?.data?.brilleseddel.venstreSylinder} />
               </Brødtekst>
             </Kolonne>
           </Rad>
@@ -144,9 +144,9 @@ export const RegistrerSøknadLesevisning: React.FC = () => {
             § 2 Bestillingen må inneholde glass
           </Heading>
           <Brødtekst>Inneholder bestillingen glass?</Brødtekst>
-          <Brødtekst>{capitalize(vilkårsgrunnlag?.komplettBrille.vilkårOppfylt)}</Brødtekst>
-          {vilkårsgrunnlag?.komplettBrille.vilkårOppfylt === VilkårsResultat.NEI && (
-            <Brødtekst>{vilkårsgrunnlag?.komplettBrille.begrunnelse}</Brødtekst>
+          <Brødtekst>{capitalize(vilkårsgrunnlag?.data?.komplettBrille.vilkårOppfylt)}</Brødtekst>
+          {vilkårsgrunnlag?.data?.komplettBrille.vilkårOppfylt === VilkårsResultat.NEI && (
+            <Brødtekst>{vilkårsgrunnlag?.data?.komplettBrille.begrunnelse}</Brødtekst>
           )}
         </Avstand>
 
@@ -155,9 +155,9 @@ export const RegistrerSøknadLesevisning: React.FC = () => {
             § 2 Brillen må være bestilt hos optiker
           </Heading>
           <Brødtekst>Er brillen bestilt hos optiker?</Brødtekst>
-          <Brødtekst>{capitalize(vilkårsgrunnlag?.bestiltHosOptiker.vilkårOppfylt)}</Brødtekst>
-          {vilkårsgrunnlag?.bestiltHosOptiker.vilkårOppfylt === VilkårsResultat.NEI && (
-            <Brødtekst>{vilkårsgrunnlag?.bestiltHosOptiker.begrunnelse}</Brødtekst>
+          <Brødtekst>{capitalize(vilkårsgrunnlag?.data?.bestiltHosOptiker.vilkårOppfylt)}</Brødtekst>
+          {vilkårsgrunnlag?.data?.bestiltHosOptiker.vilkårOppfylt === VilkårsResultat.NEI && (
+            <Brødtekst>{vilkårsgrunnlag?.data?.bestiltHosOptiker.begrunnelse}</Brødtekst>
           )}
         </Avstand>
 

f888d2eb51847f1fbbfdae652d50eb69f1ede619
matsbyfl <mats@byfuglien.net>
2024-01-04 14:28:52 +0100

    Tester for tabnavigering i søknadsbildet

--- client/cypress/e2e/2_saksbehandling_soknad.cy.ts
+++ client/cypress/e2e/2_saksbehandling_soknad.cy.ts
@@ -5,18 +5,6 @@ import { clearIndexDb, plukkSak } from './testUtils.cy'
 
 describe('Saksbehandling søknad', () => {
   beforeEach(() => {
-    /*cy.setCookie(
-      'hotsak',
-      `dev-cookie.${btoa(
-        JSON.stringify({
-          name: 'Silje Saksbehandler',
-          NAVident: 'S112233',
-          email: 'dev@nav.no',
-          oid: '23ea7485-1324-4b25-a763-assdfdfa',
-          groups: ['gruppe1', 'gruppe3'],
-        })
-      )}.ignored-part`
-      )*/
     clearIndexDb()
   })
 
@@ -87,24 +75,29 @@ describe('Saksbehandling søknad', () => {
     })
   })
 
-  /*it('burde kunne overføre til Gosys', () => {
-    cy.visit('/sak/222222/hjelpemidler')
-    cy.get('[data-cy="btn-tildel-sak-222222"]').click()
-    cy.get('[data-cy="btn-vis-gosys-modal"]').should('have.text', 'Overfør til Gosys').click()
-    cy.get('h1').contains('Overfør til Gosys').should('be.visible')
-    cy.get('p')
-      .contains(
-        'Hvis du overfører saken til Gosys, vil den dukke opp som en vanlig journalføringsoppgave. Journalføring og videre saksbehandling må gjøres manuelt i Gosys og Infotrygd.'
-      )
-      .should('be.visible')
-    cy.get('[data-cy="overfor-soknad-arsak-0"]').click()
-    cy.get('[data-cy="btn-overfor-soknad"]').should('have.text', 'Overfør til Gosys').click()
-    cy.get('[data-cy="tag-soknad-status"]').should('have.text', 'Overført til Gosys')
-    /*
-    cy.get('[data-cy="alert-vedtak-status"]').should(
-      'contain.text',
-      'Saken er overført til Gosys. Videre saksbehandling skjer i Gosys'
-    )
-    
-  })*/
+  it('Navigering mellom tabs i søknadsbildet', () => {
+    plukkSak('1005')
+
+    const tabs = cy.findAllByRole('tablist').first()
+
+    tabs.within(() => {
+      cy.findByRole('tab', { name: /bruker/i }).click()
+    })
+
+    cy.findByRole('heading', { level: 1, name: /hjelpemiddelbruker/i }).should('exist')
+
+    tabs.within(() => {
+      cy.findByRole('tab', { name: /formidler/i }).click()
+    })
+
+    cy.findByRole('heading', { level: 1, name: /formidler og opplæringsansvarlig/i }).should('exist')
+
+    cy.findAllByRole('tablist')
+      .filter(':contains("Utlånsoversikt")')
+      .within(() => {
+        cy.findByRole('tab', { name: /utlånsoversikt/i }).click()
+      })
+
+    cy.findAllByRole('listitem').filter(':contains("UTLÅNSOVERSIKT")').should('exist')
+  })
 })

672046d676e15a3164daa59543ce85dd8c727e27
Erik Eiesland <erik.eiesland@nav.no>
2024-01-31 09:08:08 +0100

    retter url i mock, fjerner mock for gradering

--- client/src/mocks/handlers/personoversikt.ts
+++ client/src/mocks/handlers/personoversikt.ts
@@ -3,22 +3,12 @@ import { rest } from 'msw'
 import type { StoreHandlersFactory } from '../data'
 
 export const personoversiktHandlers: StoreHandlersFactory = ({ personStore }) => [
-  rest.post<{ brukersFodselsnummer: string }>(`/api/personinfo`, async (req, res, ctx) => {
+  rest.post<{ brukersFodselsnummer: string }>(`/api/person`, async (req, res, ctx) => {
     const { brukersFodselsnummer } = await req.json()
     const person = await personStore.hent(brukersFodselsnummer)
     if (!person) {
       return res(ctx.status(404), ctx.text('Person ikke funnet'))
     }
     return res(ctx.delay(1000), ctx.status(200), ctx.json(person))
-  }),
-  rest.post<{ brukersFodselsnummer: string }>(`/api/personinfo/tilgangsattributter`, async (req, res, ctx) => {
-    return res(
-      ctx.delay(500),
-      ctx.status(200),
-      ctx.json({
-        //adressebeskyttelseGradering: [Adressebeskyttelse.FORTROLIG],
-        erSkjermetPerson: false,
-      })
-    )
   }),
 ]

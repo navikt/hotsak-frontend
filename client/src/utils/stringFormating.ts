fb98a0b958ab6eef9cc2750aefca0bcf68abdb54
Erik Eiesland <erik.eiesland@nav.no>
2024-02-20 08:29:59 +0100

    sørger for at det ikke blir et ekstra mellomrom i formaterte navn

--- client/src/utils/stringFormating.ts
+++ client/src/utils/stringFormating.ts
@@ -1,5 +1,5 @@
 import type { Navn } from '../types/types.internal'
-import { isString } from './type'
+import { isNavn, isString } from './type'
 
 export const capitalize = (value?: string): string => {
   if (!value) {
@@ -7,34 +7,35 @@ export const capitalize = (value?: string): string => {
   } else return value.charAt(0).toUpperCase() + value.toLowerCase().slice(1)
 }
 
-export const capitalizeName = (value: string | Navn) => {
+export const capitalizeName = (value: string | Navn): string => {
   const lowercaseValue = formatName(value).toLowerCase()
 
   const storBokstavEtterBindestrek = capitalizeMedSkilletegn(lowercaseValue, '-')
   return capitalizeMedSkilletegn(storBokstavEtterBindestrek, ' ')
 }
 
-function capitalizeMedSkilletegn(value: string, skilletegn: string) {
+function capitalizeMedSkilletegn(value: string, skilletegn: string): string {
   return value
     .split(skilletegn)
     .map((v) => v.charAt(0).toUpperCase() + v.slice(1))
     .join(skilletegn)
 }
-export const formaterKontonummer = (kontonummer?: string) => {
+export const formaterKontonummer = (kontonummer?: string): string => {
   return kontonummer ? `${kontonummer?.slice(0, 4)}.${kontonummer?.slice(4, 6)}.${kontonummer?.slice(6)}` : ''
 }
 
-export const formaterFødselsnummer = (fødselsnummer: string) => {
+export const formaterFødselsnummer = (fødselsnummer: string): string => {
   return `${fødselsnummer.slice(0, 6)} ${fødselsnummer.slice(6)}`
 }
 
 export const formatName = (navn: string | Navn): string => {
   if (isString(navn)) return navn
-  const { fornavn, mellomnavn, etternavn } = navn
-  return capitalizeName(`${fornavn} ${mellomnavn ? `${mellomnavn} ` : ''} ${etternavn}`)
+  if (!isNavn(navn)) return ''
+  const fulltNavn = [navn.fornavn, navn.mellomnavn, navn.etternavn].filter((value) => !!value).join(' ')
+  return capitalizeName(fulltNavn)
 }
 
-export const formaterTelefonnummer = (telefon: string) => {
+export const formaterTelefonnummer = (telefon: string): string => {
   const siffer = telefon.split('')
 
   return `${siffer.slice(0, 2).join('')} ${siffer.slice(2, 4).join('')} ${siffer.slice(4, 6).join('')} ${siffer

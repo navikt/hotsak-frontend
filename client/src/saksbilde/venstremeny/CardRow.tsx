3f695814e90b8697a3b178a2ef58ac707dda0979
Erik Eiesland <erik.eiesland@nav.no>
2024-05-06 08:18:07 +0200

    tester med venstrejusterte kopieringsknapper

--- client/src/saksbilde/venstremeny/CardRow.tsx
+++ client/src/saksbilde/venstremeny/CardRow.tsx
@@ -1,5 +1,5 @@
 import { isValidElement, ReactNode } from 'react'
-import { CopyButton, HGrid, HGridProps } from '@navikt/ds-react'
+import { CopyButton, HGrid, HGridProps, HStack } from '@navikt/ds-react'
 import styled from 'styled-components'
 import { Tekst } from '../../felleskomponenter/typografi'
 
@@ -11,8 +11,21 @@ export interface CardRowProps {
   columns?: HGridProps['columns']
 }
 
+const hStack = true
+
 export function CardRow(props: CardRowProps) {
   const { children, icon, copyText, align, columns } = props
+
+  if (hStack) {
+    return (
+      <HStack gap="2" align={align || (copyText ? 'center' : align)} wrap={false}>
+        <Ikon>{icon}</Ikon>
+        {isValidElement(children) ? children : <Tekst>{children}</Tekst>}
+        {copyText && <CopyButton copyText={copyText} size="small" />}
+      </HStack>
+    )
+  }
+
   return (
     <HGrid gap="2" align={align} columns={columns || '1.25rem auto 1.25rem'}>
       <Ikon>{icon}</Ikon>

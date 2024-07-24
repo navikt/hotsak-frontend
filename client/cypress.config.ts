fa9c416382d9717aa8e60d8a12df3b2490a5761c
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2024-07-24 16:10:48 +0200

    Increase cypress timeout since the pages load a bit slowly during the tests

--- client/cypress.config.ts
+++ client/cypress.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    // setupNodeEvents(on, config) {
+    //   // implement node event listeners here
+    // },
     baseUrl: 'http://localhost:3001',
     viewportWidth: 1200,
     viewportHeight: 660,
+    defaultCommandTimeout: 20000,
   },
 })

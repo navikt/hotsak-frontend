f5550b6be58f874001a9f62e4d5ef1e71abb8db6
Håkon <hakonhagen2@gmail.com>
2024-08-15 10:06:50 +0200

    La til nytt hasteårsak: UTSKRIVING_FRA_SYKEHUS_SOM_IKKE_KAN_PLANLEGGES_V3
    Endret også tekst for hasteårsak `ANNET`

--- client/src/saksbilde/hjelpemidler/Hastesak.tsx
+++ client/src/saksbilde/hjelpemidler/Hastesak.tsx
@@ -40,6 +40,8 @@ const tekstByHasteårsak: Record<keyof typeof Hasteårsak, string> = {
   [Hasteårsak.UTSKRIVING_FRA_SYKEHUS_SOM_IKKE_KAN_PLANLEGGES]: 'Utskriving fra sykehus som ikke kan planlegges.',
   [Hasteårsak.UTSKRIVING_FRA_SYKEHUS_SOM_IKKE_KAN_PLANLEGGES_V2]:
     'Innbygger skal skrives ut fra sykehus uten at det var mulig å planlegge. Det er derfor behov for hjelpemidler til stell og pleie slik at utskrivingen kan gjennomføres.',
+  [Hasteårsak.UTSKRIVING_FRA_SYKEHUS_SOM_IKKE_KAN_PLANLEGGES_V3]:
+    'Innbygger skal skrives ut fra sykehus eller rehabilitering uten at det var mulig å planlegge. Det er derfor behov for hjelpemidler til stell og pleie slik at utskrivingen kan gjennomføres.',
   [Hasteårsak.RASK_FORVERRING_AV_ALVORLIG_DIAGNOSE]: 'Det har skjedd en rask forverring av en alvorlig diagnose.',
-  [Hasteårsak.ANNET]: 'Annet, plutselig oppstått behov på grunn av brukers helsesituasjon.',
+  [Hasteårsak.ANNET]: 'Annen årsak til plutselig oppstått behov på grunn av brukers helsesituasjon.',
 }

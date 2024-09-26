358cc3cd060cdb3afab4acf59dad3e72d5e18705
matsbyfl <mats@byfuglien.net>
2024-09-26 14:24:16 +0200

    Henter døddato fra person og ikke bruker i saksregler

--- client/src/saksregler/useSaksregler.ts
+++ client/src/saksregler/useSaksregler.ts
@@ -1,13 +1,16 @@
+import { usePerson } from '../personoversikt/usePerson.ts'
 import { useSak } from '../saksbilde/useSak.ts'
 import { useSaksbehandlerErTildeltSak } from '../tilgang/useSaksbehandlerErTildeltSak.ts'
 
 export function useSaksregler() {
   const { data: sak } = useSak()?.sak ?? { data: undefined }
+  const { personInfo: person } = usePerson(sak?.bruker.fnr)
+
   const saksbehandlerErTildeltSak = useSaksbehandlerErTildeltSak(sak)
   return {
     sakId: sak?.sakId,
     kanHenleggeSak(): boolean {
-      return !!(saksbehandlerErTildeltSak && sak?.bruker?.dødsdato)
+      return !!(saksbehandlerErTildeltSak && person?.dødsdato)
     },
   }
 }

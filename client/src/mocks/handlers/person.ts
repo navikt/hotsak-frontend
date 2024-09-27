9c65004233385f9e27ae81d5287e09b41cc1bbae
matsbyfl <mats@byfuglien.net>
2024-09-27 12:50:42 +0200

    Fjerner hook for henting av OEBS adresser som ikke brukes

--- client/src/mocks/handlers/person.ts
+++ client/src/mocks/handlers/person.ts
@@ -2,43 +2,16 @@ import { delay, http, HttpResponse } from 'msw'
 
 import type { StoreHandlersFactory } from '../data'
 import { respondNotFound } from './response'
-import type { OebsAdresse } from '../../personoversikt/useOebsAdresser'
 
 export const personHandlers: StoreHandlersFactory = ({ personStore }) => [
   http.post<never, { brukersFodselsnummer: string }>(`/api/person`, async ({ request }) => {
     const { brukersFodselsnummer } = await request.json()
     const person = await personStore.hent(brukersFodselsnummer)
-    await delay(1000)
+    await delay(500)
+
     if (!person) {
       return respondNotFound()
     }
     return HttpResponse.json(person)
-  }),
-
-  http.post<never, { brukersFodselsnummer: string }, OebsAdresse[]>(`/api/person/oebs-adresser`, async () => {
-    await delay(100)
-    const adresser: OebsAdresse[] = [
-      {
-        brukernummer: '1',
-        leveringsadresse: {
-          adresse: 'Gateadresse 1',
-          postnummer: '4600',
-          poststed: 'Kristiansand S',
-          kommunenummer: 'Kristiansand',
-        },
-        primæradresse: '',
-      },
-      {
-        brukernummer: '1',
-        leveringsadresse: {
-          adresse: 'Gateadresse 2',
-          postnummer: '4601',
-          poststed: 'Kristiansand S',
-          kommunenummer: 'Kristiansand',
-        },
-        primæradresse: '',
-      },
-    ]
-    return HttpResponse.json(adresser)
   }),
 ]

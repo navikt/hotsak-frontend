d116d5ed6f6ce8179ca35329c7d8d42d838bfdf0
Håkon <hakonhagen2@gmail.com>
2024-09-27 08:49:15 +0200

    Må vise bytteårsak også

--- client/src/saksbilde/hjelpemidler/Bytter.tsx
+++ client/src/saksbilde/hjelpemidler/Bytter.tsx
@@ -1,6 +1,7 @@
 import { HStack } from '@navikt/ds-react'
-import { Bytte } from '../../types/types.internal'
+import { Fragment } from 'react'
 import { Etikett } from '../../felleskomponenter/typografi'
+import { Bytte, BytteÅrsak } from '../../types/types.internal'
 
 interface Props {
   bytter: Bytte[]
@@ -10,21 +11,38 @@ const Bytter = ({ bytter }: Props) => {
   return (
     <>
       {bytter.map((bytte, i) => (
-        <HStack gap="2" key={i}>
-          <Etikett>{bytte.erTilsvarende ? 'Skal byttes inn' : 'Skal leveres tilbake'}</Etikett>
-          <div>
-            {bytte.hmsnr} {bytte.hjmNavn}
-            {bytte.serienr && (
-              <>
-                <br />
-                Serienr: {bytte.serienr}
-              </>
-            )}
-          </div>
-        </HStack>
+        <Fragment key={i}>
+          <HStack gap="2">
+            <Etikett>{bytte.erTilsvarende ? 'Skal byttes inn' : 'Skal leveres tilbake'}</Etikett>
+            <div>
+              {bytte.hmsnr} {bytte.hjmNavn}
+              {bytte.serienr && (
+                <>
+                  <br />
+                  Serienr: {bytte.serienr}
+                </>
+              )}
+            </div>
+          </HStack>
+          {bytte.årsak && (
+            <HStack gap="2" key={i}>
+              <Etikett>Begrunnelse for bytte</Etikett>
+              <div>Hjelpemiddelet skal byttes fordi det er {tekstByBytteårsak[bytte.årsak]}</div>
+            </HStack>
+          )}
+        </Fragment>
       ))}
     </>
   )
+}
+
+const tekstByBytteårsak: Record<keyof typeof BytteÅrsak, string> = {
+  [BytteÅrsak.UTSLITT]: 'utslitt',
+  [BytteÅrsak.VOKST_FRA]: 'vokst fra',
+  [BytteÅrsak.ENDRINGER_I_INNBYGGERS_FUNKSJON]:
+    'endringer i innbyggers funksjon slik at hjelpemidlet ikke lenger dekker behovet.',
+  [BytteÅrsak.FEIL_STØRRELSE]: 'FEIL_STØRRELSE',
+  [BytteÅrsak.VURDERT_SOM_ØDELAGT_AV_LOKAL_TEKNIKER]: 'vurdert som ødelagt av lokal tekniker.',
 }
 
 export default Bytter

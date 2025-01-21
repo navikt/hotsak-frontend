213d0707595668efe8a086abfa3b48a8037b9b4b
Mats Byfuglien <mats.byfuglien@nav.no>
2025-01-21 13:52:06 +0100

    Bruker inline alert på hast

--- client/src/saksbilde/hjelpemidlerNyLayout/Hast.tsx
+++ client/src/saksbilde/hjelpemidlerNyLayout/Hast.tsx
@@ -1,4 +1,4 @@
-import { Box, List, VStack } from '@navikt/ds-react'
+import { Alert, Box, List, VStack } from '@navikt/ds-react'
 import { BrytbarBrødtekst } from '../../felleskomponenter/typografi.tsx'
 import { Hasteårsak, Hast as HastType } from '../../types/BehovsmeldingTypes.ts'
 
@@ -7,29 +7,21 @@ export function Hast(props: { hast?: HastType }) {
   if (!hast) return null
   const { hasteårsaker, hastBegrunnelse } = hast
   return (
-    <Box
-      background="surface-subtle"
-      paddingBlock="4 2"
-      paddingInline="4"
-      borderRadius="medium"
-      borderColor="border-subtle"
-      borderWidth="1"
-    >
-      <VStack gap="4">
-        <Box>
-          <List size="small" headingTag="h2" title=" Formidler har markert saken som hast, begrunnelse:">
-            {hasteårsaker.map((årsak) => (
-              <List.Item key={årsak}>
-                <BrytbarBrødtekst>{tekstByHasteårsak[årsak]}</BrytbarBrødtekst>
-                {årsak === Hasteårsak.ANNET && hastBegrunnelse && (
-                  <BrytbarBrødtekst>{hastBegrunnelse}</BrytbarBrødtekst>
-                )}
-              </List.Item>
-            ))}
-          </List>
-        </Box>
-      </VStack>
-    </Box>
+    <VStack gap="4">
+      <Alert variant="warning" size="small" inline>
+        Hast: Formidler har markert saken som hast.
+      </Alert>
+      <Box paddingInline="8 0">
+        <List size="small" title="Årsak til at det haster">
+          {hasteårsaker.map((årsak) => (
+            <List.Item key={årsak}>
+              <BrytbarBrødtekst>{tekstByHasteårsak[årsak]}</BrytbarBrødtekst>
+              {årsak === Hasteårsak.ANNET && hastBegrunnelse && <BrytbarBrødtekst>{hastBegrunnelse}</BrytbarBrødtekst>}
+            </List.Item>
+          ))}
+        </List>
+      </Box>
+    </VStack>
   )
 }
 

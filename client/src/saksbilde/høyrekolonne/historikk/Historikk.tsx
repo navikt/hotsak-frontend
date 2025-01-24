fe3f13033cde2fdfd88feca0f3d224c4d95b6e19
Mats Byfuglien <mats.byfuglien@nav.no>
2025-01-24 14:00:20 +0100

    Legger på gap mellom innslag i historik kolonna

--- client/src/saksbilde/høyrekolonne/historikk/Historikk.tsx
+++ client/src/saksbilde/høyrekolonne/historikk/Historikk.tsx
@@ -14,7 +14,7 @@ export function Historikk() {
       loading={isLoading && 'Henter historikk...'}
     >
       {hendelser.length > 0 ? (
-        <VStack as={ListeUtenPunkt}>
+        <VStack as={ListeUtenPunkt} gap="2">
           {hendelser.map((it) => (
             <HistorikkHendelse key={it.id} {...it} />
           ))}

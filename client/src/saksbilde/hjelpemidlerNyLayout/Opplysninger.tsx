5e6f9096f73dedcb54deff7a784b477db82f9d55
Mats Byfuglien <mats.byfuglien@nav.no>
2025-01-30 11:02:21 +0100

    Viser kun opplysninger som kulepunkter hvis det er flere innholdelementer på opplysningen

--- client/src/saksbilde/hjelpemidlerNyLayout/Opplysninger.tsx
+++ client/src/saksbilde/hjelpemidlerNyLayout/Opplysninger.tsx
@@ -8,15 +8,25 @@ export function Opplysninger({ opplysninger }: { opplysninger: Opplysning[] }) {
     return (
       <Box key={opplysning.ledetekst.nb}>
         <Etikett>{`${storForbokstavIOrd(opplysning.ledetekst.nb)}`}</Etikett>
-        <ul key={opplysning.ledetekst.nb} style={{ margin: '0' }}>
-          {opplysning.innhold.map((element, idx) => (
-            <li key={idx}>
-              <Brødtekst>
-                {element.forhåndsdefinertTekst ? element.forhåndsdefinertTekst.nb : element.fritekst}
-              </Brødtekst>
-            </li>
-          ))}
-        </ul>
+        {opplysning.innhold.length === 1 ? (
+          <>
+            <Brødtekst>
+              {opplysning.innhold[0].forhåndsdefinertTekst
+                ? opplysning.innhold[0].forhåndsdefinertTekst.nb
+                : opplysning.innhold[0].fritekst}
+            </Brødtekst>
+          </>
+        ) : (
+          <ul key={opplysning.ledetekst.nb} style={{ margin: '0' }}>
+            {opplysning.innhold.map((element, idx) => (
+              <li key={idx}>
+                <Brødtekst>
+                  {element.forhåndsdefinertTekst ? element.forhåndsdefinertTekst.nb : element.fritekst}
+                </Brødtekst>
+              </li>
+            ))}
+          </ul>
+        )}
       </Box>
     )
   })

82110fef9f2e8a1228affc78fb85a1b23bf9bfe9
Mats Byfuglien <mats.byfuglien@nav.no>
2025-01-30 17:00:01 +0100

    Fikser avstand mellom flere opplysninger på tilbehør

--- client/src/saksbilde/hjelpemidlerNyLayout/Opplysninger.tsx
+++ client/src/saksbilde/hjelpemidlerNyLayout/Opplysninger.tsx
@@ -1,33 +1,37 @@
-import { Box } from '@navikt/ds-react'
+import { Box, VStack } from '@navikt/ds-react'
 import { Brødtekst, Etikett } from '../../felleskomponenter/typografi'
 import { Opplysning } from '../../types/BehovsmeldingTypes'
 import { storForbokstavIOrd } from '../../utils/formater'
 
 export function Opplysninger({ opplysninger }: { opplysninger: Opplysning[] }) {
-  return opplysninger.map((opplysning) => {
-    return (
-      <Box key={opplysning.ledetekst.nb}>
-        <Etikett>{`${storForbokstavIOrd(opplysning.ledetekst.nb)}`}</Etikett>
-        {opplysning.innhold.length === 1 ? (
-          <>
-            <Brødtekst>
-              {opplysning.innhold[0].forhåndsdefinertTekst
-                ? opplysning.innhold[0].forhåndsdefinertTekst.nb
-                : opplysning.innhold[0].fritekst}
-            </Brødtekst>
-          </>
-        ) : (
-          <ul key={opplysning.ledetekst.nb} style={{ margin: '0' }}>
-            {opplysning.innhold.map((element, idx) => (
-              <li key={idx}>
+  return (
+    <VStack gap="3">
+      {opplysninger.map((opplysning) => {
+        return (
+          <Box key={opplysning.ledetekst.nb}>
+            <Etikett>{`${storForbokstavIOrd(opplysning.ledetekst.nb)}`}</Etikett>
+            {opplysning.innhold.length === 1 ? (
+              <>
                 <Brødtekst>
-                  {element.forhåndsdefinertTekst ? element.forhåndsdefinertTekst.nb : element.fritekst}
+                  {opplysning.innhold[0].forhåndsdefinertTekst
+                    ? opplysning.innhold[0].forhåndsdefinertTekst.nb
+                    : opplysning.innhold[0].fritekst}
                 </Brødtekst>
-              </li>
-            ))}
-          </ul>
-        )}
-      </Box>
-    )
-  })
+              </>
+            ) : (
+              <ul key={opplysning.ledetekst.nb} style={{ margin: '0' }}>
+                {opplysning.innhold.map((element, idx) => (
+                  <li key={idx}>
+                    <Brødtekst>
+                      {element.forhåndsdefinertTekst ? element.forhåndsdefinertTekst.nb : element.fritekst}
+                    </Brødtekst>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </Box>
+        )
+      })}
+    </VStack>
+  )
 }

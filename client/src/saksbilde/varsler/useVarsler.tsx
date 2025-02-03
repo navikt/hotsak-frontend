3bc92fe754bcf80c1892ebd0ee47d8fd6af00a08
Mats Byfuglien <mats.byfuglien@nav.no>
2025-02-03 10:52:34 +0100

    Endrer rekkefølge på varsler for søknad

--- client/src/saksbilde/varsler/useVarsler.tsx
+++ client/src/saksbilde/varsler/useVarsler.tsx
@@ -29,16 +29,16 @@ export function useSøknadsVarsler(): VarslerDataResponse {
     beskrivelser.push('Det er levering til en annen adresse. Denne må registreres.')
   }
 
+  if (harAnnenKontaktperson) {
+    varslerFor.push(VarselFor.ANNEN_KONTAKTPERSON)
+    beskrivelser.push('Det er en annen kontaktperson enn hjelpemiddelformidler. Denne må registreres.')
+  }
+
   if (harBeskjedTilKommune) {
     varslerFor.push(VarselFor.BESKJED_TIL_KOMMUNE)
     beskrivelser.push(
       'Det er en beskjed til kommunen. Du må sjekke at beskjeden ikke inneholder personopplysninger eller annen sensitiv informasjon, og legge den inn på SF i OeBS.'
     )
-  }
-
-  if (harAnnenKontaktperson) {
-    varslerFor.push(VarselFor.ANNEN_KONTAKTPERSON)
-    beskrivelser.push('Det er en annen kontaktperson enn hjelpemiddelformidler. Denne må registreres.')
   }
 
   const varsler = [{ tittel: 'Du må fullføre SF i OeBS. Følgende må gjøres:', varslerFor, beskrivelse: beskrivelser }]

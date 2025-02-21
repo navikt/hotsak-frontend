3d397f59c384069ada15d6b2641a9c197092f309
Mats Byfuglien <mats.byfuglien@nav.no>
2025-02-21 13:48:17 +0100

    Trikser til med AD gruppe i dev for å teste lesemodus

--- client/src/oppgaveliste/useOppgavetilgang.ts
+++ client/src/oppgaveliste/useOppgavetilgang.ts
@@ -5,12 +5,17 @@ export function useOppgavetilgang(): { harSkrivetilgang: boolean } {
   const { grupper } = useInnloggetSaksbehandler()
 
   const harSkrivetilgang = useMemo(() => {
-    if (window.appSettings.MILJO === 'prod-gcp' || window.appSettings.MILJO === 'dev-gcp') {
+    if (window.appSettings.MILJO === 'prod-gcp') {
       return true
+    } else if (window.appSettings.MILJO === 'dev-gcp') {
+      // Kun midlertidig logikk for å teste om resten av logikken rundt lesemodus fungerer frem til ny policy løsning er på plass i backend
+      return !grupper.includes(Gruppe.HOTSAK_NASJONAL)
     } else {
       return grupper.includes(Gruppe.HOTSAK_SAKSBEHANDLER)
     }
   }, [grupper])
+
+  console.log('Saksbehandler har skrivetilgang til oppgave', { harSkrivetilgang })
 
   return { harSkrivetilgang }
 }

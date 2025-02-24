6dd36a62d97fe237c7b4b397fe8bb7a587b91c0e
Mats Byfuglien <mats.byfuglien@nav.no>
2025-02-24 13:44:16 +0100

    Legger til baseurl igjen

--- client/cypress.config.js
+++ client/cypress.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
     // setupNodeEvents(on, config) {
     //   // implement node event listeners here
     // },
-    // baseUrl: 'http://localhost:3001',
+    baseUrl: 'http://localhost:3001',
     viewportWidth: 1200,
     viewportHeight: 660,
     defaultCommandTimeout: 20000,

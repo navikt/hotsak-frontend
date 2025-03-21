95dc77827b787cdd79ed0b9e8c65a1b115d629ad
Mats Byfuglien <mats.byfuglien@nav.no>
2025-03-21 15:41:51 +0100

    Flytter logikk for om vi skal polle på notater eller ikke inn i notat hooken. Kjøttbolle actionmeny med linker

--- client/src/saksbilde/høyrekolonne/notat/Notater.tsx
+++ client/src/saksbilde/høyrekolonne/notat/Notater.tsx
@@ -1,7 +1,21 @@
 import { listsPlugin, MDXEditor, quotePlugin, thematicBreakPlugin } from '@mdxeditor/editor'
 import '@mdxeditor/editor/style.css'
-import { ExternalLinkIcon } from '@navikt/aksel-icons'
-import { Box, Heading, HStack, Link, Loader, ReadMore, Tag, ToggleGroup, Tooltip, VStack } from '@navikt/ds-react'
+import { ExternalLinkIcon, MenuElipsisHorizontalCircleIcon } from '@navikt/aksel-icons'
+import {
+  ActionMenu,
+  Box,
+  Button,
+  Heading,
+  HStack,
+  Link,
+  Loader,
+  ReadMore,
+  Spacer,
+  Tag,
+  ToggleGroup,
+  Tooltip,
+  VStack,
+} from '@navikt/ds-react'
 import { useState } from 'react'
 import { BrytbarBrødtekst, Brødtekst, Mellomtittel, Tekst, Undertittel } from '../../../felleskomponenter/typografi.tsx'
 import { NotatType } from '../../../types/types.internal.ts'
@@ -18,12 +32,8 @@ export interface NotaterProps {
 }
 
 export function Notater({ sakId, lesevisning }: NotaterProps) {
-  const { notater, avventerJournalføring, isLoading: notaterLaster } = useNotater(sakId)
-  const { utkast: aktiveUtkast } = useNotater(sakId, avventerJournalføring ? { refreshInterval: 2000 } : null)
+  const { notater, isLoading: notaterLaster } = useNotater(sakId)
   const [notatType, setNotatType] = useState<string>(NotatType.JOURNALFØRT.toString())
-
-  const antallInterneNotatUtkast = aktiveUtkast.filter((u) => u.type === NotatType.INTERNT)
-  const antallJournalførteNotatUtkast = aktiveUtkast.filter((u) => u.type === NotatType.JOURNALFØRT)
 
   return (
     <>
@@ -34,7 +44,7 @@ export function Notater({ sakId, lesevisning }: NotaterProps) {
         <ReadMore size="small" header="Når må du journalføre notat i saken">
           <BrytbarBrødtekst>
             Når du mottar saksopplysninger utenfra som er med på å avgjøre utfallet av en sak, skal opplysningene
-            journalføres.  Når du skriver notatet nedenfor vil vi lagre utkastet fortløpende. Når du journalfører
+            journalføres. Når du skriver notatet nedenfor vil vi lagre utkastet fortløpende. Når du journalfører
             notatet, blir det synlig for innbygger neste virkedag på innlogget side på nav.no
           </BrytbarBrødtekst>
         </ReadMore>
@@ -45,14 +55,8 @@ export function Notater({ sakId, lesevisning }: NotaterProps) {
             label="Opprett nytt notat"
             onChange={(notatType) => setNotatType(notatType)}
           >
-            <ToggleGroup.Item
-              value={NotatType.INTERNT.toString()}
-              label={`Internt notat ${antallInterneNotatUtkast.length > 0 ? '(' + antallInterneNotatUtkast.length + ')' : ''}`}
-            />
-            <ToggleGroup.Item
-              value={NotatType.JOURNALFØRT.toString()}
-              label={`Skal journalføres ${antallJournalførteNotatUtkast.length > 0 ? '(' + antallJournalførteNotatUtkast.length + ')' : ''}`}
-            />
+            <ToggleGroup.Item value={NotatType.INTERNT.toString()} label={`Internt notat`} />
+            <ToggleGroup.Item value={NotatType.JOURNALFØRT.toString()} label={`Skal journalføres`} />
           </ToggleGroup>
         </Box>
       </VStack>
@@ -80,34 +84,53 @@ export function Notater({ sakId, lesevisning }: NotaterProps) {
             {notater.map((notat) => {
               return (
                 <Box key={notat.id} background="surface-subtle" padding="3" borderRadius="xlarge">
-                  <HStack gap="2">
-                    <VStack gap="2">
-                      <div>
-                        <Tag
-                          variant={notat.type === NotatType.JOURNALFØRT ? 'alt3-filled' : 'alt3-moderate'}
-                          size="small"
-                        >
-                          {storForbokstavIOrd(notat.type)}
-                        </Tag>
-                      </div>
-                      <HStack gap="2">
-                        <Heading level="3" size="xsmall" style={{ fontSize: '1em' }}>
-                          {notat.tittel}
-                        </Heading>
-                        {notat.journalpostId && notat.dokumentId && (
-                          <Tooltip content="Åpne i ny fane">
-                            <Link href={`/api/journalpost/${notat.journalpostId}/${notat.dokumentId}`} target="_blank">
-                              <ExternalLinkIcon />
-                            </Link>
-                          </Tooltip>
-                        )}
-                      </HStack>
-                      <VStack>
-                        <Brødtekst>{formaterTidsstempelLesevennlig(notat.opprettet)}</Brødtekst>
-                        <Undertittel>{notat.saksbehandler.navn}</Undertittel>
-                      </VStack>
+                  <VStack gap="2">
+                    <HStack gap="2" wrap={false} align="center">
+                      <Tag
+                        variant={notat.type === NotatType.JOURNALFØRT ? 'alt3-filled' : 'alt3-moderate'}
+                        size="small"
+                      >
+                        {storForbokstavIOrd(notat.type)}
+                      </Tag>
+
+                      {notat.journalpostId && notat.dokumentId && (
+                        <>
+                          <Spacer />
+                          <ActionMenu>
+                            <ActionMenu.Trigger>
+                              <Button
+                                variant="tertiary-neutral"
+                                icon={<MenuElipsisHorizontalCircleIcon title="Notatmeny" />}
+                                size="small"
+                              />
+                            </ActionMenu.Trigger>
+                            <ActionMenu.Content>
+                              <ActionMenu.Item as="a" href="">
+                                Åpne som dokument <ExternalLinkIcon />
+                              </ActionMenu.Item>
+                              <ActionMenu.Item>Feilregistrer</ActionMenu.Item>
+                            </ActionMenu.Content>
+                          </ActionMenu>
+                        </>
+                      )}
+                    </HStack>
+                    <HStack gap="2">
+                      <Heading level="3" size="xsmall" style={{ fontSize: '1em' }}>
+                        {notat.tittel}
+                      </Heading>
+                      {notat.journalpostId && notat.dokumentId && (
+                        <Tooltip content="Åpne i ny fane">
+                          <Link href={`/api/journalpost/${notat.journalpostId}/${notat.dokumentId}`} target="_blank">
+                            <ExternalLinkIcon />
+                          </Link>
+                        </Tooltip>
+                      )}
+                    </HStack>
+                    <VStack>
+                      <Brødtekst>{formaterTidsstempelLesevennlig(notat.opprettet)}</Brødtekst>
+                      <Undertittel>{notat.saksbehandler.navn}</Undertittel>
                     </VStack>
-                  </HStack>
+                  </VStack>
 
                   {notat.tekst && (
                     <MardownEditorPreviewStyling>
--- client/src/saksbilde/høyrekolonne/notat/useNotater.tsx
+++ client/src/saksbilde/høyrekolonne/notat/useNotater.tsx
@@ -2,12 +2,12 @@ import useSwr from 'swr'
 import { useErNotatPilot } from '../../../state/authentication'
 import { Notat, NotatType, Saksnotater } from '../../../types/types.internal'
 import { KeyedMutator } from 'swr'
+import { useEffect, useState } from 'react'
 
 interface NotaterResponse {
   antallNotater: number
   harUtkast: boolean
   notater: Notat[]
-  avventerJournalføring: boolean
   utkast: Notat[]
   isLoading: boolean
   mutate: KeyedMutator<Saksnotater>
@@ -16,11 +16,22 @@ interface NotaterResponse {
 export function useNotater(sakId?: string, opts?: any): NotaterResponse {
   const erNotatPilot = useErNotatPilot()
 
+  const [refreshInterval, setRefreshInterval] = useState(0)
+
   const {
     data: saksnotater,
     mutate,
     isLoading,
-  } = useSwr<Saksnotater>(erNotatPilot && sakId ? `/api/sak/${sakId}/notater` : null, opts)
+  } = useSwr<Saksnotater>(erNotatPilot && sakId ? `/api/sak/${sakId}/notater` : null, { refreshInterval })
+
+  opts = {
+    ...opts,
+    refreshInterval: saksnotater?.notater.some(
+      (notat) => notat.type === NotatType.JOURNALFØRT && (!notat.journalpostId || !notat.dokumentId)
+    )
+      ? 5000
+      : 0,
+  }
 
   const { notater, totalElements } = saksnotater ?? { notater: [], totalElements: 0 }
   const ferdigstilteNotater = notater?.filter((notat) => notat.ferdigstilt) ?? []
@@ -30,11 +41,18 @@ export function useNotater(sakId?: string, opts?: any): NotaterResponse {
       .filter((notat) => notat.type === NotatType.JOURNALFØRT)
       .filter((notat) => !notat.journalpostId || !notat.dokumentId).length > 0
 
+  useEffect(() => {
+    if (avventerJournalføring) {
+      setRefreshInterval(2000)
+    } else {
+      setRefreshInterval(0)
+    }
+  }, [avventerJournalføring])
+
   return {
     antallNotater: totalElements,
     harUtkast: utkast.length > 0,
     notater: ferdigstilteNotater,
-    avventerJournalføring,
     utkast,
     isLoading,
     mutate,

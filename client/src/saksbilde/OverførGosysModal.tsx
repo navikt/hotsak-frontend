ce334c70682537f62805baf64f690be13402b84c
Mats Byfuglien <mats.byfuglien@nav.no>
2025-05-27 10:43:08 +0200

    Legger til tekst i overfør gosys modal som informerer om hva som skjer med notatene hvis det er notater tilknyttet saken

--- client/src/saksbilde/OverførGosysModal.tsx
+++ client/src/saksbilde/OverførGosysModal.tsx
@@ -1,6 +1,11 @@
+import { Alert, VStack } from '@navikt/ds-react'
 import type { Tilbakemelding } from '../innsikt/Besvarelse'
 import { SpørreundersøkelseModal } from '../innsikt/SpørreundersøkelseModal'
 import type { SpørreundersøkelseId } from '../innsikt/spørreundersøkelser'
+import { useNotater } from './høyrekolonne/notat/useNotater'
+import { useSakId } from './useSak'
+import { Brødtekst } from '../felleskomponenter/typografi'
+import { NotatType } from '../types/types.internal'
 
 export interface OverførGosysModalProps {
   open: boolean
@@ -12,6 +17,11 @@ export interface OverførGosysModalProps {
 }
 
 export function OverførGosysModal({ open, loading, spørreundersøkelseId, onBekreft, onClose }: OverførGosysModalProps) {
+  const sakId = useSakId()
+  const { notater } = useNotater(sakId)
+
+  if (notater.length > 0) {
+  }
   return (
     <SpørreundersøkelseModal
       open={open}
@@ -21,6 +31,26 @@ export function OverførGosysModal({ open, loading, spørreundersøkelseId, onBe
       knappetekst="Overfør til Gosys"
       onBesvar={onBekreft}
       onClose={onClose}
-    />
+    >
+      {notater.length > 0 && (
+        <Alert variant="info" size="small">
+          <VStack gap="3">
+            {notater.find((notat) => notat.type === NotatType.JOURNALFØRT) && (
+              <Brødtekst>
+                Journalførte notater feilregistreres ved overføring av sak til Gosys. Hvis du har behov for å knytte
+                feilregistrert notat til ny sak i Gosys, kan dere inntil videre ta kontakt med DigiHoT på Teams kanalen
+                "DigiHoT - innspill, spørsmål og info" for bistand.
+              </Brødtekst>
+            )}
+            {notater.find((notat) => notat.type === NotatType.INTERNT) && (
+              <Brødtekst>
+                Interne notater i saken overføres ikke til Gosys. Hvis du har behov for å overføre disse til oppgaven i
+                Gosys, må det gjøres mannuelt.
+              </Brødtekst>
+            )}
+          </VStack>
+        </Alert>
+      )}
+    </SpørreundersøkelseModal>
   )
 }

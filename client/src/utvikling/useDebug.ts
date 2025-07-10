0fff0f764f31efd7078f9841bce9b1220e85eb41
Erik Eiesland <erik.eiesland@nav.no>
2025-07-10 13:22:57 +0200

    fjerner noen ubrukte komponenter

--- client/src/utvikling/useDebug.ts
+++
@@ -1,9 +0,0 @@
-import { useDebugValue, useEffect } from 'react'
-import { logDebug } from './logDebug'
-
-export function useDebug(value: any): void {
-  useDebugValue(value)
-  useEffect(() => {
-    logDebug(value)
-  }, [value])
-}

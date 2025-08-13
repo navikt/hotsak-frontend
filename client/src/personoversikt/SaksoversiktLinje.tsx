dab1e9336b8cd01aab23f4c253bc02e9300a7b86
Mats Byfuglien <mats.byfuglien@nav.no>
2025-08-13 15:01:57 +0200

    Setter value på tabs i saksoversikt

--- client/src/personoversikt/SaksoversiktLinje.tsx
+++ client/src/personoversikt/SaksoversiktLinje.tsx
@@ -2,6 +2,7 @@ import { Tabs } from '@navikt/ds-react'
 
 import { TabLink } from '../saksbilde/TabLink'
 import { spacingVar } from '../felleskomponenter/Avstand'
+import { useLocation } from 'react-router'
 
 interface SaksoversiktLinjeProps {
   sakerCount: number
@@ -9,8 +10,9 @@ interface SaksoversiktLinjeProps {
 }
 
 export function SaksoversiktLinje({ sakerCount, hjelpemidlerCount }: SaksoversiktLinjeProps) {
+  const location = useLocation()
   return (
-    <Tabs>
+    <Tabs value={location.pathname}>
       <Tabs.List style={{ padding: `0 ${spacingVar(8)}` }}>
         <TabLink to={`/personoversikt/saker`}>{`Saker (${sakerCount})`}</TabLink>
         <TabLink to={`/personoversikt/hjelpemidler`}>{`Utlånsoversikt (${hjelpemidlerCount})`}</TabLink>

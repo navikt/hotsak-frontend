778f4d37f60c4f09a09cba33da6a7a9a2e0e52eb
Erik Eiesland <erik.eiesland@nav.no>
2025-08-20 08:51:22 +0200

    retter noen SWR-nøkler som manglet slash

--- client/src/saksbilde/mutateSak.ts
+++ client/src/saksbilde/mutateSak.ts
@@ -1,5 +1,5 @@
 import { mutate } from 'swr'
 
 export function mutateSak(sakId: any): Promise<Awaited<any>[]> {
-  return Promise.all([mutate(`api/sak/${sakId}`), mutate(`api/sak/${sakId}/historikk`)])
+  return Promise.all([mutate(`/api/sak/${sakId}`), mutate(`/api/sak/${sakId}/historikk`)])
 }

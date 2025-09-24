6f02748811d9fced70a298a20ba84286e6c698af
Mats Byfuglien <mats.byfuglien@nav.no>
2025-09-24 09:15:34 +0200

    Bytter ut deprecated title i List med heading

--- client/src/saksbilde/bestillingsordning/Saksvarsler.tsx
+++ client/src/saksbilde/bestillingsordning/Saksvarsler.tsx
@@ -1,11 +1,14 @@
-import { List } from '@navikt/ds-react'
+import { Heading, List } from '@navikt/ds-react'
 import { SystemAlert } from '../../felleskomponenter/SystemAlert'
 import { Varsel } from '../../types/types.internal'
 
 export function Saksvarsler({ varsler }: { varsler: Varsel[] }) {
   return varsler.map((varsel) => (
     <SystemAlert key={varsel.tittel} variant="info">
-      <List size="small" as="ul" title={varsel.tittel}>
+      <Heading level="2" size="xsmall" spacing>
+        {varsel.tittel}
+      </Heading>
+      <List size="small" as="ul">
         {varsel.beskrivelse.map((beskrivelse) => (
           <List.Item key={beskrivelse}>{beskrivelse}</List.Item>
         ))}

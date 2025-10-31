a8e5efeb519773eba05ff5a26ed854605a6d3fda
Erik Eiesland <erik.eiesland@nav.no>
2025-10-31 13:08:45 +0100

    v3 -> v2

--- client/src/oppgaveliste/Oppgaveliste.tsx
+++ client/src/oppgaveliste/Oppgaveliste.tsx
@@ -2,11 +2,11 @@ import { lazy } from 'react'
 import { Route, Routes } from 'react-router'
 
 import { useNyOppgaveliste } from './useNyOppgaveliste.ts'
-import { OppgavelisteTabs } from './v3/OppgavelisteTabs.tsx'
+import { OppgavelisteTabs } from './v2/OppgavelisteTabs.tsx'
 
 const OppgavelisteV1 = lazy(() => import('./v1/OppgavelisteV1.tsx'))
-const MineOppgaverWrapper = lazy(() => import('./v3/MineOppgaverWrapper.tsx'))
-const EnhetensOppgaverWrapper = lazy(() => import('./v3/EnhetensOppgaverWrapper.tsx'))
+const MineOppgaverWrapper = lazy(() => import('./v2/MineOppgaverWrapper.tsx'))
+const EnhetensOppgaverWrapper = lazy(() => import('./v2/EnhetensOppgaverWrapper.tsx'))
 
 export default function Oppgaveliste() {
   const [nyOppgaveliste] = useNyOppgaveliste()
--- client/src/oppgaveliste/v2/MappeTag.tsx
+++ client/src/oppgaveliste/v2/MappeTag.tsx
@@ -1,6 +1,5 @@
 import { Tag } from '@navikt/ds-react'
 
-
 export function MappeTag({ mappenavn }: { mappenavn: string }) {
   return (
     <Tag size="small" variant="alt1">

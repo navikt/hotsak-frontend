3b68574245a108d778565cf18053a9b93dcc6a4b
Erik Eiesland <erik.eiesland@nav.no>
2025-11-03 11:45:59 +0100

    loading på "Last inn flere"-knapp

--- client/src/oppgaveliste/v2/EnhetensOppgaver.tsx
+++ client/src/oppgaveliste/v2/EnhetensOppgaver.tsx
@@ -6,14 +6,14 @@ import { OppgaveTable } from './OppgaveTable.tsx'
 import { useEnhetensOppgaver } from './useEnhetensOppgaver.ts'
 
 export function EnhetensOppgaver() {
-  const { oppgaver, lastInnFlere, totalElements } = useEnhetensOppgaver(30)
+  const { oppgaver, totalElements, isLoading, lastInnFlere } = useEnhetensOppgaver(30)
   return (
     <>
       <Box margin="5">
         <OppgaveFilter oppgavetyper={oppgavetyper} gjelder={gjelder} oppgaveprioritet={oppgaveprioritet} />
         <OppgaveTable oppgaver={oppgaver} />
         <HStack align="center" gap="3" justify="center" marginBlock="5">
-          <Button size="small" type="button" variant="secondary" onClick={lastInnFlere}>
+          <Button size="small" type="button" variant="secondary" loading={isLoading} onClick={lastInnFlere}>
             Last inn flere
           </Button>
           <BodyShort>{`Viser ${oppgaver.length} av ${totalElements} oppgaver`}</BodyShort>

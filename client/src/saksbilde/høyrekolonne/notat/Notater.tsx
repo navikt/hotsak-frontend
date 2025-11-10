a9a9c2b52fc0d04b7fd8501c01ff02709e5c1ecb
Mats Byfuglien <mats.byfuglien@nav.no>
2025-11-10 22:23:51 +0100

    Fjerner debug logging igjen

--- client/src/saksbilde/høyrekolonne/notat/Notater.tsx
+++ client/src/saksbilde/høyrekolonne/notat/Notater.tsx
@@ -32,8 +32,6 @@ export function Notater({ sakId, lesevisning }: NotaterProps) {
 
   const aktivtUtkast = finnAktivtUtkast(notatType)
 
-  console.log('Aktivt utkast', aktivtUtkast)
-
   const filterOptions: FilterOption[] = [
     { key: 'ALLE', label: 'Alle' },
     { key: NotatType.INTERNT, label: 'Internt arbeidsnotat' },
--- client/src/saksbilde/høyrekolonne/notat/markdown/Lagreindikator.tsx
+++ client/src/saksbilde/høyrekolonne/notat/markdown/Lagreindikator.tsx
@@ -4,8 +4,6 @@ import { formaterTidsstempelKort } from '../../../../utils/dato'
 export function Lagreindikator(props: { lagrerUtkast: boolean; sistLagretTidspunkt?: string }) {
   const { lagrerUtkast, sistLagretTidspunkt } = props
 
-  console.log('sistLagretTidspunkt', sistLagretTidspunkt)
-
   if (!sistLagretTidspunkt) {
     return <div style={{ marginLeft: 'auto', marginTop: '2rem' }}></div>
   }

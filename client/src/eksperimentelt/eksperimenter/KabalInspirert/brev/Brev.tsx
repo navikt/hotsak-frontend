dc1588b2892e4c630b9962c251d2badcf71c7a84
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2025-11-11 13:40:19 +0100

    Enkel prototype av forhåndsvisning av brevet (fungerer i msw, ikke i dev)

--- client/src/eksperimentelt/eksperimenter/KabalInspirert/brev/Brev.tsx
+++ client/src/eksperimentelt/eksperimenter/KabalInspirert/brev/Brev.tsx
@@ -1,10 +1,11 @@
-import { Alert, Loader } from '@navikt/ds-react'
+import { Alert, Button, Loader, Tag } from '@navikt/ds-react'
 import useSWR from 'swr'
 import Breveditor, { StateMangement } from './breveditor/Breveditor.tsx'
 import { useMemo, useState } from 'react'
 import { useSak } from '../../../../saksbilde/useSak.ts'
 import { BrevmalVelger } from './brevmaler/Brevmaler.tsx'
 import { formaterDatoLang } from '../../../../utils/dato.ts'
+import { CheckmarkIcon, MenuElipsisVerticalCircleIcon, PadlockUnlockedIcon } from '@navikt/aksel-icons'
 
 export const Brev = () => {
   const { sak } = useSak()
@@ -12,7 +13,9 @@ export const Brev = () => {
   const brevutkast = useSWR<
     {
       error?: string
-      data?: StateMangement
+      data?: StateMangement & {
+        markertKlart: boolean
+      }
       opprettet: string
     },
     Error
@@ -29,51 +32,143 @@ export const Brev = () => {
     return <Alert variant="warning">Brev ikke tilgjengelig.</Alert>
   }
 
+  const lagreBrevutkast = async (data: any) => {
+    return fetch(`/api/sak/${sak!.data.sakId}/brevutkast`, {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Requested-With': 'XMLHttpRequest',
+      },
+      body: JSON.stringify({
+        brevtype: 'BREVEDITOR_VEDTAKSBREV',
+        målform: 'BOKMÅL',
+        data: data,
+      }),
+    })
+  }
+
+  const makerKlart = async (klart: boolean) => {
+    if (brevutkast.data?.data?.value) {
+      await lagreBrevutkast({ ...brevutkast.data.data, markertKlart: klart })
+      await brevutkast.mutate()
+    }
+  }
+
   return (
     <>
       {errorEr404 && valgtMal === undefined && <BrevmalVelger velgMal={velgMal} />}
       {(!errorEr404 || valgtMal !== undefined) && brevutkast.data && (
         <div style={{ height: '100%' }}>
-          <Breveditor
-            placeholder="Skriv et fantastisk brev her..."
-            metadata={{
-              brukersNavn: sak?.data.bruker.fulltNavn || '',
-              brukersFødselsnummer: sak?.data.bruker.fnr || '',
-              saksnummer: Number(sak!.data.sakId),
-              brevOpprettet: formaterDatoLang(brevutkast.data?.opprettet),
-              saksbehandlerNavn: sak?.data.saksbehandler?.navn || '',
-              attestantsNavn: undefined,
-              hjelpemiddelsentral: sak?.data.enhet.enhetsnavn || 'Nav hjelpemiddelsentral',
-            }}
-            brevId={sak!.data.sakId.toString()}
-            templateMarkdown={valgtMal}
-            initialState={brevutkast.data?.data}
-            onLagreBrev={async (state) => {
-              await fetch(`/api/sak/${sak!.data.sakId}/brevutkast`, {
-                method: 'post',
-                headers: {
-                  'Content-Type': 'application/json',
-                  'X-Requested-With': 'XMLHttpRequest',
-                },
-                body: JSON.stringify({
-                  brevtype: 'BREVEDITOR_VEDTAKSBREV',
-                  målform: 'BOKMÅL',
-                  data: state,
-                }),
-              }).then((res) => {
-                if (!res.ok) throw new Error(`Brev ikke lagret, statuskode ${res.status}`)
-              })
-            }}
-            onSlettBrev={async () => {
-              velgMal(undefined) // Unngå at forrige valgte mal trigger at breveditoren laster den på nytt
-              await fetch(`/api/sak/${sak!.data.sakId}/brevutkast/BREVEDITOR_VEDTAKSBREV`, {
-                method: 'delete',
-              }).then((res) => {
-                if (!res.ok) throw new Error(`Brev ikke slettet, statuskode ${res.status}`)
-              })
-              await brevutkast.mutate()
-            }}
-          />
+          {!brevutkast.data?.data?.markertKlart && (
+            <>
+              <div
+                style={{
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                  padding: '0em 1em',
+                  alignContent: 'center',
+                  height: '3.5em',
+                }}
+              >
+                <Tag variant="warning-moderate" style={{ margin: '1em 0' }}>
+                  Delvis innvilget
+                </Tag>
+                <div
+                  style={{
+                    alignContent: 'center',
+                    justifyContent: 'space-between',
+                    gap: '1em',
+                    display: 'flex',
+                    margin: '1em 0',
+                  }}
+                >
+                  <Button
+                    variant="primary"
+                    size="small"
+                    icon={<CheckmarkIcon aria-hidden />}
+                    onClick={() => makerKlart(true)}
+                  >
+                    Marker som klart
+                  </Button>
+                  <Button variant="tertiary-neutral" icon={<MenuElipsisVerticalCircleIcon aria-hidden />} />
+                </div>
+              </div>
+              <Breveditor
+                placeholder="Skriv et fantastisk brev her..."
+                metadata={{
+                  brukersNavn: sak?.data.bruker.fulltNavn || '',
+                  brukersFødselsnummer: sak?.data.bruker.fnr || '',
+                  saksnummer: Number(sak!.data.sakId),
+                  brevOpprettet: formaterDatoLang(brevutkast.data?.opprettet),
+                  saksbehandlerNavn: sak?.data.saksbehandler?.navn || '',
+                  attestantsNavn: undefined,
+                  hjelpemiddelsentral: sak?.data.enhet.enhetsnavn || 'Nav hjelpemiddelsentral',
+                }}
+                brevId={sak!.data.sakId.toString()}
+                templateMarkdown={valgtMal}
+                initialState={brevutkast.data?.data}
+                onLagreBrev={async (state) => {
+                  await lagreBrevutkast(state).then((res) => {
+                    if (!res.ok) throw new Error(`Brev ikke lagret, statuskode ${res.status}`)
+                  })
+                }}
+                onSlettBrev={async () => {
+                  velgMal(undefined) // Unngå at forrige valgte mal trigger at breveditoren laster den på nytt
+                  await fetch(`/api/sak/${sak!.data.sakId}/brevutkast/BREVEDITOR_VEDTAKSBREV`, {
+                    method: 'delete',
+                  }).then((res) => {
+                    if (!res.ok) throw new Error(`Brev ikke slettet, statuskode ${res.status}`)
+                  })
+                  await brevutkast.mutate()
+                }}
+              />
+            </>
+          )}
+          {brevutkast.data?.data?.markertKlart && (
+            <>
+              <div
+                style={{
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                  padding: '0em 1em',
+                  alignContent: 'center',
+                  height: '3.5em',
+                }}
+              >
+                <Tag variant="warning-moderate" style={{ margin: '1em 0' }}>
+                  Delvis innvilget
+                </Tag>
+                <Tag variant="success-moderate" style={{ margin: '1em 0' }}>
+                  Brev klart
+                </Tag>
+                <div
+                  style={{
+                    alignContent: 'center',
+                    justifyContent: 'space-between',
+                    gap: '1em',
+                    display: 'flex',
+                    margin: '1em 0',
+                  }}
+                >
+                  <Button
+                    variant="tertiary-neutral"
+                    size="small"
+                    icon={<PadlockUnlockedIcon aria-hidden />}
+                    onClick={() => makerKlart(false)}
+                  >
+                    Gjenåpne brevutkast
+                  </Button>
+                </div>
+              </div>
+              <iframe
+                src={`/api/sak/${sak?.data.sakId}/brev/BREVEDITOR_VEDTAKSBREV#toolbar=0&navpanes=0&zoom=FitV`}
+                width="100%"
+                height="100%"
+                allow="fullscreen"
+                style={{ border: 'none' }}
+              />
+            </>
+          )}
         </div>
       )}
     </>

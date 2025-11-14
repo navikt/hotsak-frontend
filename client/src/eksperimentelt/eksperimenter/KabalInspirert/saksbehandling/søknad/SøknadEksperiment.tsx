06cd0d3c47c93f0712400b963c9382eb2085eed6
Mats Byfuglien <mats.byfuglien@nav.no>
2025-11-14 21:36:37 +0100

    Tester ut "kollapse" av ulike deler av søknadspanelet

--- client/src/eksperimentelt/eksperimenter/KabalInspirert/saksbehandling/søknad/SøknadEksperiment.tsx
+++ client/src/eksperimentelt/eksperimenter/KabalInspirert/saksbehandling/søknad/SøknadEksperiment.tsx
@@ -1,20 +1,21 @@
-import { Box, Label, VStack } from '@navikt/ds-react'
-import { memo, useMemo } from 'react'
+import { ChevronDownIcon, ChevronUpIcon } from '@navikt/aksel-icons'
+import { Box, Button, HStack, Label, VStack } from '@navikt/ds-react'
+import { memo, useMemo, useState } from 'react'
+import { OebsAlert } from '../../../../../saksbilde/hjelpemidler/OebsAlert'
 import {
   ingenAlternativeProdukterForHmsArtNr,
   useAlternativeProdukter,
   useProduktLagerInfo,
 } from '../../../../../saksbilde/hjelpemidler/useAlternativeProdukter'
+import { useArtiklerForSak } from '../../../../../saksbilde/hjelpemidler/useArtiklerForSak'
 import { useHjelpemiddelprodukter } from '../../../../../saksbilde/hjelpemidler/useHjelpemiddelprodukter'
 import { BehovsmeldingType, Innsenderbehovsmelding } from '../../../../../types/BehovsmeldingTypes'
 import { Sak } from '../../../../../types/types.internal'
 import { BrukersFunksjonEksperiment } from './BrukersFunksjonEksperiment'
+import { HastEksperiment } from './HastEksperiment'
 import { HjelpemiddelEksperiment } from './HjelpemiddelEksperiment'
 import { SummertFrittståendTilbehørEksperiment, SummertHjelpemidlerEksperiment } from './SummeringEksperiment'
 import { FrittStåendeTilbehørEksperiment } from './TilbehørListeEksperiment'
-import { HastEksperiment } from './HastEksperiment'
-import { OebsAlert } from '../../../../../saksbilde/hjelpemidler/OebsAlert'
-import { useArtiklerForSak } from '../../../../../saksbilde/hjelpemidler/useArtiklerForSak'
 
 interface SøknadEksperimentProps {
   sak: Sak
@@ -41,6 +42,8 @@ function SøknadEksperiment({ sak, behovsmelding }: SøknadEksperimentProps) {
   }, [hjelpemidler])
 
   const { data: hjelpemiddelprodukter } = useHjelpemiddelprodukter(alleHmsNr)
+  const [skjulteHjelpemidler, setSkjulteHjelpemidler] = useState(false)
+  const [skjulteTilbehør, setSkjulteTilbehør] = useState(false)
   const { alternativeProdukterByHmsArtNr, harOppdatertLagerstatus } = useAlternativeProdukter(alleHjelpemidler)
   const { produkter: lagerinfoForProdukter } = useProduktLagerInfo(alleHmsNr)
   const { artikler } = useArtiklerForSak(sak.sakId)
@@ -57,46 +60,66 @@ function SøknadEksperiment({ sak, behovsmelding }: SøknadEksperimentProps) {
 
       {hjelpemidler.length > 0 && (
         <VStack paddingBlock="space-20 0">
-          <Label size="small" as="h2" textColor="subtle" spacing={false}>
-            HJELPEMIDLER
-          </Label>
+          <HStack align="center">
+            <Button
+              variant="tertiary"
+              size="small"
+              icon={skjulteHjelpemidler ? <ChevronUpIcon /> : <ChevronDownIcon />}
+              onClick={() => setSkjulteHjelpemidler(!skjulteHjelpemidler)}
+            />
+            <Label size="small" as="h2" textColor="subtle" spacing={false}>
+              HJELPEMIDLER
+            </Label>
+          </HStack>
+
           <SummertHjelpemidlerEksperiment hjelpemidler={hjelpemidler} />
         </VStack>
       )}
 
-      {hjelpemidler.map((hjelpemiddel) => (
-        <Box.New
-          key={hjelpemiddel.produkt.hmsArtNr}
-          paddingInline="space-12"
-          background="neutral-softA"
-          borderColor="neutral-subtle"
-          borderWidth="1"
-          borderRadius="large"
-        >
-          <HjelpemiddelEksperiment
-            sak={sak}
-            hjelpemiddel={hjelpemiddel}
-            produkter={hjelpemiddelprodukter}
-            minmaxStyrt={
-              lagerinfoForProdukter[hjelpemiddel.produkt.hmsArtNr]?.wareHouseStock?.some((l) => l?.minmax === true) ||
-              false
-            }
-            alternativeProdukter={
-              alternativeProdukterByHmsArtNr[hjelpemiddel.produkt.hmsArtNr] ?? ingenAlternativeProdukterForHmsArtNr
-            }
-            harOppdatertLagerstatus={harOppdatertLagerstatus}
-          />
-        </Box.New>
-      ))}
+      {!skjulteHjelpemidler &&
+        hjelpemidler.map((hjelpemiddel) => (
+          <Box.New
+            key={hjelpemiddel.produkt.hmsArtNr}
+            paddingInline="space-12"
+            background="neutral-softA"
+            borderColor="neutral-subtle"
+            borderWidth="1"
+            borderRadius="large"
+          >
+            <HjelpemiddelEksperiment
+              sak={sak}
+              hjelpemiddel={hjelpemiddel}
+              produkter={hjelpemiddelprodukter}
+              minmaxStyrt={
+                lagerinfoForProdukter[hjelpemiddel.produkt.hmsArtNr]?.wareHouseStock?.some((l) => l?.minmax === true) ||
+                false
+              }
+              alternativeProdukter={
+                alternativeProdukterByHmsArtNr[hjelpemiddel.produkt.hmsArtNr] ?? ingenAlternativeProdukterForHmsArtNr
+              }
+              harOppdatertLagerstatus={harOppdatertLagerstatus}
+            />
+          </Box.New>
+        ))}
       {tilbehør && tilbehør.length > 0 && (
         <>
           <VStack paddingBlock="space-20 0">
-            <Label size="small" as="h2" textColor="subtle" spacing={false}>
-              TILBEHØR
-            </Label>
+            <HStack align="center">
+              <Button
+                variant="tertiary"
+                size="small"
+                icon={skjulteTilbehør ? <ChevronUpIcon /> : <ChevronDownIcon />}
+                onClick={() => setSkjulteTilbehør(!skjulteTilbehør)}
+              />
+              <Label size="small" as="h2" textColor="subtle" spacing={false}>
+                TILBEHØR
+              </Label>
+            </HStack>
             <SummertFrittståendTilbehørEksperiment tilbehør={tilbehør} />
           </VStack>
-          <FrittStåendeTilbehørEksperiment tilbehør={tilbehør} produkter={hjelpemiddelprodukter} />
+          {!skjulteTilbehør && (
+            <FrittStåendeTilbehørEksperiment tilbehør={tilbehør} produkter={hjelpemiddelprodukter} />
+          )}
         </>
       )}
       {funksjonsbeskrivelse && <BrukersFunksjonEksperiment funksjonsbeskrivelse={funksjonsbeskrivelse} />}

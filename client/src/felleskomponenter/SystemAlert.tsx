50d81cae82b31106aa6d8cef31bae6a8dd0a978d
Mats Byfuglien <mats.byfuglien@nav.no>
2025-12-11 23:35:33 +0100

    Venstrejusterer OeBS alert

--- client/src/felleskomponenter/SystemAlert.tsx
+++ client/src/felleskomponenter/SystemAlert.tsx
@@ -1,12 +1,11 @@
 import { InfoCard } from '@navikt/ds-react'
 import { ReactNode } from 'react'
-import { TextContainer } from './typografi'
-import sharedStyles from '../styles/shared.module.css'
 import styles from './systemalert.module.css'
+import { TextContainer } from './typografi'
 
 export function SystemAlert({ title, children }: { title: string; children?: ReactNode }) {
   return (
-    <TextContainer className={`${sharedStyles.center} ${styles.container}`}>
+    <TextContainer className={`${styles.container}`}>
       <InfoCard data-color="info" size="small">
         <InfoCard.Header>
           <InfoCard.Title>{title}</InfoCard.Title>

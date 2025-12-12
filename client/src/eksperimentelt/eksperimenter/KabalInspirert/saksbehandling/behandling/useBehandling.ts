732d6060bfd46e36c04b6be2f87c9005b8da3f7c
Mats Byfuglien <mats.byfuglien@nav.no>
2025-12-12 15:03:15 +0100

    Henter kun behandlinger i miljøer hvor msw er aktivert

--- client/src/eksperimentelt/eksperimenter/KabalInspirert/saksbehandling/behandling/useBehandling.ts
+++ client/src/eksperimentelt/eksperimenter/KabalInspirert/saksbehandling/behandling/useBehandling.ts
@@ -2,6 +2,7 @@ import useSwr, { KeyedMutator } from 'swr'
 
 import { useSakId } from '../../../../../saksbilde/useSak.ts'
 import { BehandlingerForSak } from '../../../../../types/behandlingTyper.ts'
+import { useMiljø } from '../../../../../utils/useMiljø.ts'
 
 export interface Dataesponse extends BehandlingerForSak {
   error: any
@@ -11,12 +12,16 @@ export interface Dataesponse extends BehandlingerForSak {
 
 export function useBehandling(): Dataesponse {
   const sakId = useSakId()
+
+  const { mswAktivert } = useMiljø()
+
+  console.log(`useBehandling kjører kun hvis msw er aktivert frem til API er klart mswAktivert:  ${mswAktivert}`)
   const {
     data: behandling,
     error,
     isLoading,
     mutate,
-  } = useSwr<Dataesponse>(sakId ? `/api/sak/${sakId}/behandling` : null)
+  } = useSwr<Dataesponse>(mswAktivert && sakId ? `/api/sak/${sakId}/behandling` : null)
 
   return {
     behandlinger: behandling?.behandlinger || [],
--- client/src/utils/useMiljø.ts
+++ client/src/utils/useMiljø.ts
@@ -2,14 +2,18 @@ import { useMemo } from 'react'
 
 export function useMiljø() {
   const miljø = window.appSettings.NAIS_CLUSTER_NAME
+  const mswAktivert = window.appSettings.USE_MSW || false
+
   return useMemo(() => {
     const erLocal = miljø === 'local'
     const erDev = miljø === 'dev-gcp'
+
     return {
       erLocal,
       erDev,
       erProd: miljø === 'prod-gcp',
       erIkkeProd: erLocal || erDev,
+      mswAktivert,
     }
   }, [miljø])
 }

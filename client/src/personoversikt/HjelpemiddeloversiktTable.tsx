6b11b62b88ab05148b8155b28544663b458a1456
Erik Eiesland <erik.eiesland@nav.no>
2025-12-17 14:18:58 +0100

    columns som konstanter

--- client/src/personoversikt/HjelpemiddeloversiktTable.tsx
+++ client/src/personoversikt/HjelpemiddeloversiktTable.tsx
@@ -1,5 +1,4 @@
 import { Link } from '@navikt/ds-react'
-import { useMemo } from 'react'
 
 import { DataGrid, type DataGridColumn } from '../felleskomponenter/data/DataGrid.tsx'
 import { Skjermlesertittel } from '../felleskomponenter/typografi'
@@ -11,52 +10,6 @@ export interface HjelpemiddeloversiktTableProps {
 }
 
 export function HjelpemiddeloversiktTable({ artikler, loading }: HjelpemiddeloversiktTableProps) {
-  const columns: DataGridColumn<HjelpemiddelArtikkel>[] = useMemo(
-    () => [
-      {
-        field: 'datoUtsendelse',
-        header: 'Utlånsdato',
-        width: 110,
-        formatDate: true,
-      },
-      {
-        field: 'grunndataKategoriKortnavn',
-        header: 'Kategori',
-        width: 200,
-      },
-      {
-        field: 'produkt',
-        header: 'Produkt',
-        renderCell(row) {
-          return row.grunndataProduktNavn || row.beskrivelse
-        },
-      },
-      {
-        field: 'antall',
-        header: 'Antall',
-        width: 90,
-        renderCell(row) {
-          return `${row.antall} ${row.antallEnhet.toLowerCase()}`
-        },
-      },
-      {
-        field: 'hmsnr',
-        header: 'HMS-nummer',
-        width: 150,
-        renderCell(row) {
-          return row.hjelpemiddeldatabasenURL ? (
-            <Link href={row.hjelpemiddeldatabasenURL} target="_blank">
-              {row.hmsnr}
-            </Link>
-          ) : (
-            row.hmsnr
-          )
-        },
-      },
-    ],
-    []
-  )
-
   return (
     <>
       <Skjermlesertittel level="2">Utlånsoversikt</Skjermlesertittel>
@@ -73,3 +26,46 @@ export function HjelpemiddeloversiktTable({ artikler, loading }: Hjelpemiddelove
     </>
   )
 }
+
+const columns: ReadonlyArray<DataGridColumn<HjelpemiddelArtikkel>> = [
+  {
+    field: 'datoUtsendelse',
+    header: 'Utlånsdato',
+    width: 110,
+    formatDate: true,
+  },
+  {
+    field: 'grunndataKategoriKortnavn',
+    header: 'Kategori',
+    width: 200,
+  },
+  {
+    field: 'produkt',
+    header: 'Produkt',
+    renderCell(row) {
+      return row.grunndataProduktNavn || row.beskrivelse
+    },
+  },
+  {
+    field: 'antall',
+    header: 'Antall',
+    width: 90,
+    renderCell(row) {
+      return `${row.antall} ${row.antallEnhet.toLowerCase()}`
+    },
+  },
+  {
+    field: 'hmsnr',
+    header: 'HMS-nummer',
+    width: 150,
+    renderCell(row) {
+      return row.hjelpemiddeldatabasenURL ? (
+        <Link href={row.hjelpemiddeldatabasenURL} target="_blank">
+          {row.hmsnr}
+        </Link>
+      ) : (
+        row.hmsnr
+      )
+    },
+  },
+]

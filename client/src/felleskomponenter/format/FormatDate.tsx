8cb0fe0774d2d72ba00e56b7d720b6c74a282c1d
Erik Eiesland <erik.eiesland@nav.no>
2026-02-02 10:17:37 +0100

    white-space nowrap for FormatDate og FormatDateTime

--- client/src/felleskomponenter/format/FormatDate.tsx
+++ client/src/felleskomponenter/format/FormatDate.tsx
@@ -1,11 +1,21 @@
+import { useMemo } from 'react'
+
+import classes from './FormatDate.module.css'
+
 export function FormatDate({ date }: { date?: string }) {
-  if (!date) {
+  const formatted = useMemo(() => (date ? formatter.format(new Date(date)) : null), [date])
+  if (!formatted) {
     return null
   }
-  const formatted = new Date(date).toLocaleDateString('nb-NO', {
-    year: 'numeric',
-    month: '2-digit',
-    day: '2-digit',
-  })
-  return <time dateTime={date}>{formatted}</time>
+  return (
+    <time className={classes.root} dateTime={date}>
+      {formatted}
+    </time>
+  )
 }
+
+const formatter = new Intl.DateTimeFormat('nb-NO', {
+  year: 'numeric',
+  month: '2-digit',
+  day: '2-digit',
+})
--- client/src/felleskomponenter/format/FormatDateTime.tsx
+++ client/src/felleskomponenter/format/FormatDateTime.tsx
@@ -1,13 +1,23 @@
+import { useMemo } from 'react'
+
+import classes from './FormatDateTime.module.css'
+
 export function FormatDateTime({ dateTime }: { dateTime?: string }) {
-  if (!dateTime) {
+  const formatted = useMemo(() => (dateTime ? formatter.format(new Date(dateTime)) : null), [dateTime])
+  if (!formatted) {
     return null
   }
-  const formatted = new Date(dateTime).toLocaleDateString('nb-NO', {
-    year: 'numeric',
-    month: '2-digit',
-    day: '2-digit',
-    hour: '2-digit',
-    minute: '2-digit',
-  })
-  return <time dateTime={dateTime}>{formatted}</time>
+  return (
+    <time className={classes.root} dateTime={dateTime}>
+      {formatted}
+    </time>
+  )
 }
+
+const formatter = new Intl.DateTimeFormat('nb-NO', {
+  year: 'numeric',
+  month: '2-digit',
+  day: '2-digit',
+  hour: '2-digit',
+  minute: '2-digit',
+})

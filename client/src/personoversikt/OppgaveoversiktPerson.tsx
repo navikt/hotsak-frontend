90df2b21de559a34844c2cdf476f442ecd6e8276
Erik Eiesland <erik.eiesland@nav.no>
2026-02-02 16:32:26 +0100

    pinner @platejs/slate, @platejs/floating og clsx
    fikk warning om feil versjon av @platejs/slate:
    WARN Issues with peer dependencies found
    .
    └─┬ @platejs/floating 52.0.11
      └── ✕ unmet peer platejs@>=52.0.11: found 52.0.1
    
    legger til @dnd-kit/core for drag-and-drop

---
+++ client/src/personoversikt/OppgaveoversiktPerson.tsx
@@ -0,0 +1,24 @@
+import { DataGrid } from '../felleskomponenter/data/DataGrid.tsx'
+import { useOpppgavesøk } from '../oppgave/useOppgavesøk.ts'
+import { oppgaveColumns } from '../oppgaveliste/v2/oppgaveColumns.tsx'
+import { selectOppgaveId } from '../oppgaveliste/v2/oppgaveSelectors.ts'
+
+export interface OppgaveoversiktPersonProps {
+  fnr: string
+}
+
+export function OppgaveoversiktPerson(props: OppgaveoversiktPersonProps) {
+  const { fnr } = props
+  const { data } = useOpppgavesøk({ brukerId: fnr })
+  if (!data) {
+    return null
+  }
+  return <DataGrid rows={data.oppgaver} columns={columns} keyFactory={selectOppgaveId} />
+}
+
+const columns = [
+  oppgaveColumns.åpneOppgave,
+  oppgaveColumns.oppgavetype,
+  oppgaveColumns.behandlingstema,
+  oppgaveColumns.behandlingstype,
+]
--- client/src/personoversikt/PersonoversiktTabs.tsx
+++ client/src/personoversikt/PersonoversiktTabs.tsx
@@ -4,24 +4,24 @@ import { useLocation } from 'react-router'
 import { Eksperiment } from '../felleskomponenter/Eksperiment.tsx'
 import { TabLink } from '../saksbilde/TabLink'
 
-interface SaksoversiktLinjeProps {
+export interface PersonoversiktTabsProps {
   sakerCount: number
   hjelpemidlerCount: number
 }
 
-export function SaksoversiktLinje({ sakerCount, hjelpemidlerCount }: SaksoversiktLinjeProps) {
+export function PersonoversiktTabs({ sakerCount, hjelpemidlerCount }: PersonoversiktTabsProps) {
   const location = useLocation()
   return (
     <Tabs value={location.pathname}>
       <Tabs.List style={{ padding: `0 var(--ax-space-16)` }}>
         <Eksperiment>
-          <TabLink to={`/personoversikt/oppgaver`}>{`Oppgaver`}</TabLink>
+          <TabLink to="/personoversikt/oppgaver">Oppgaver</TabLink>
         </Eksperiment>
-        <TabLink to={`/personoversikt/saker`}>{`Saker (${sakerCount})`}</TabLink>
+        <TabLink to="/personoversikt/saker">{`Saker (${sakerCount})`}</TabLink>
         <Eksperiment>
-          <TabLink to={`/personoversikt/dokumenter`}>{`Dokumenter`}</TabLink>
+          <TabLink to="/personoversikt/dokumenter">Dokumenter</TabLink>
         </Eksperiment>
-        <TabLink to={`/personoversikt/hjelpemidler`}>{`Utlånsoversikt (${hjelpemidlerCount})`}</TabLink>
+        <TabLink to="/personoversikt/hjelpemidler">{`Utlånsoversikt (${hjelpemidlerCount})`}</TabLink>
       </Tabs.List>
     </Tabs>
   )

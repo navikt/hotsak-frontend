624d09436d7951f8f275b4620dd409481260a838
Mats Byfuglien <mats.byfuglien@nav.no>
2026-02-18 15:51:03 +0100

    Wip oppgraderinger til Aksel v8

--- client/src/global.d.ts
+++ client/src/global.d.ts
@@ -2,4 +2,4 @@ declare type ID = string | number
 declare type Maybe<T> = T | undefined
 declare type Nullable<T> = T | null
 
-declare module '@navikt/ds-css/darkside'
+declare module '@navikt/ds-css'

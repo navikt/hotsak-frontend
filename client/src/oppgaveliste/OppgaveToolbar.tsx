ffea1d8d0364168c28eb470acc827d66e45ec4a5
Erik Eiesland <erik.eiesland@nav.no>
2026-03-19 13:49:53 +0100

    bytter switch med knapp som viser alle hastesaker

--- client/src/oppgaveliste/OppgaveToolbar.tsx
+++ client/src/oppgaveliste/OppgaveToolbar.tsx
@@ -1,9 +1,8 @@
 import { TrashIcon } from '@navikt/aksel-icons'
-import { BodyShort, Box, Button, HGrid, HStack, Switch } from '@navikt/ds-react'
+import { BodyShort, Box, Button, HGrid, HStack, Tag } from '@navikt/ds-react'
 import { type ReactNode } from 'react'
 
 import {
-  useDataGridFilterContext,
   useDataGridFilterDispatch,
   useDataGridFilterResetAllHandler,
   useIsDataGridFiltered,
@@ -11,8 +10,6 @@ import {
 import { OppgaveColumnMenu } from './OppgaveColumnMenu.tsx'
 import classes from './OppgaveToolbar.module.css'
 import type { OppgaveColumnField } from './oppgaveColumns.tsx'
-import { hasAny } from '../utils/array.ts'
-import { emptyDataGridFilterValues } from '../felleskomponenter/data/DataGridFilter.ts'
 
 export interface OppgaveToolbarProps {
   text: string
@@ -22,29 +19,37 @@ export interface OppgaveToolbarProps {
 
 export function OppgaveToolbar(props: OppgaveToolbarProps) {
   const { text, antallHastesaker = 0, children } = props
-  const { prioritet: prioritetFilterState = emptyDataGridFilterValues } = useDataGridFilterContext<OppgaveColumnField>()
   const dispatch = useDataGridFilterDispatch<OppgaveColumnField>()
   const isDataGridFiltered = useIsDataGridFiltered()
   const handleFilterResetAll = useDataGridFilterResetAllHandler()
   return (
     <Box borderColor="neutral-subtleA" borderWidth="0 0 2 0" className={classes.root} padding="space-8">
       <HGrid columns="1fr 1fr" align="center" className={classes.grid}>
-        <BodyShort size="small">{text}</BodyShort>
-        <HStack gap="space-12" justify="end" align="center">
-          {antallHastesaker > 0 && (
-            <Switch
-              data-color="warning"
-              size="small"
-              checked={hasAny(prioritetFilterState.values, hastesakValues)}
-              onChange={(event) => {
-                dispatch({
-                  type: 'singleField',
-                  field: 'prioritet',
-                  values: event.target.checked ? hastesakValues : [],
-                })
-              }}
-            >{`Vis hastesaker (${antallHastesaker})`}</Switch>
+        <HStack gap="space-12" align="center" justify="start" wrap={false}>
+          <BodyShort size="small">{text}</BodyShort>
+          {antallHastesaker > 0 ? (
+            <>
+              <Button
+                size="xsmall"
+                type="button"
+                variant="primary"
+                data-color="warning"
+                onClick={() => {
+                  dispatch({
+                    type: 'singleField',
+                    field: 'prioritet',
+                    values: hastesakValues,
+                  })
+                }}
+              >{`Vis hastesaker (${antallHastesaker})`}</Button>
+            </>
+          ) : (
+            <Tag variant="success" size="small">
+              Ingen hastesaker.
+            </Tag>
           )}
+        </HStack>
+        <HStack gap="space-12" align="center" justify="end" wrap={false}>
           {children}
           <Button
             data-color="neutral"

e10ff101239733ac748616d03a65c504d1aafe44
murvold <mads.severin.murvold@nav.no>
2026-04-24 15:34:20 +0200

    Fjerner dev-sjekk for angring

--- client/src/saksbilde/SaksbildeMenu.tsx
+++ client/src/saksbilde/SaksbildeMenu.tsx
@@ -4,16 +4,15 @@ import { useState } from 'react'
 
 import { SpørreundersøkelseId } from '../innsikt/spørreundersøkelser.ts'
 import { OppgaveMenu } from '../oppgave/OppgaveMenu.tsx'
+import { OppgaveMenuModals } from '../oppgave/OppgaveMenuModals.tsx'
 import { useOppgave } from '../oppgave/useOppgave.ts'
 import { useOppgaveregler } from '../oppgave/useOppgaveregler.ts'
+import { AngreVedtakModal } from '../sak/v2/angreVedtak/AngreVedtakModal.tsx'
 import { AngringLåst, GjenståendeOverfør, UtfallLåst } from '../sak/v2/behandling/behandlingTyper.ts'
+import { useBehandling } from '../sak/v2/behandling/useBehandling.ts'
 import { OverførtilGosysValideringFeil } from '../sak/v2/modaler/OverførtilGosysValideringFeil.tsx'
 import { OverførSakTilGosysModal } from './OverførSakTilGosysModal.tsx'
 import { useOverførSakTilGosys } from './useOverførSakTilGosys.ts'
-import { OppgaveMenuModals } from '../oppgave/OppgaveMenuModals.tsx'
-import { useBehandling } from '../sak/v2/behandling/useBehandling.ts'
-import { AngreVedtakModal } from '../sak/v2/angreVedtak/AngreVedtakModal.tsx'
-import { useMiljø } from '../utils/useMiljø.ts'
 
 export interface SaksbildeMenuProps {
   gjenståendeFørOverføring?: GjenståendeOverfør[]
@@ -27,7 +26,6 @@ export function SaksbildeMenu({ spørreundersøkelseId, gjenståendeFørOverfør
   const [visValideringsfeil, setVisValideringsfeil] = useState(false)
   const { gjeldendeBehandling } = useBehandling()
   const [angreVedtakModalOpen, setAngreVedtakModalOpen] = useState(false)
-  const { erDev } = useMiljø()
 
   if (!oppgave) {
     return null
@@ -66,7 +64,7 @@ export function SaksbildeMenu({ spørreundersøkelseId, gjenståendeFørOverfør
               >
                 Overfør sak til Gosys
               </ActionMenu.Item>
-              {gjeldendeBehandling?.utfallLåst?.includes(UtfallLåst.MIDLERTIDIG_FERDIGSTILT) && erDev && (
+              {gjeldendeBehandling?.utfallLåst?.includes(UtfallLåst.MIDLERTIDIG_FERDIGSTILT) && (
                 <ActionMenu.Item
                   disabled={gjeldendeBehandling.operasjoner.angreVedtak.angringLåst.includes(
                     AngringLåst.ANGRE_TID_UTLØPT

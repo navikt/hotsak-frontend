5e13655299826f7a3b84aa691b415130aacdb670
Erik Eiesland <erik.eiesland@nav.no>
2026-05-15 09:19:47 +0200

    feat: oppdatert informasjon om de ulike notattypene
    https://trello.com/c/G62CCZMb

--- client/src/sak/notat/Notatinformasjon.tsx
+++ client/src/sak/notat/Notatinformasjon.tsx
@@ -4,18 +4,21 @@ export function Notatinformasjon() {
   return (
     <ReadMore size="small" header="Når skal du bruke de ulike notattypene">
       <Label size="small" as="div">
-        Internt arbeidsnotat
+        Kommentarer
       </Label>
       <BodyLong size="small" spacing>
-        Brukes for egne notater, for eksempel huskelapper til deg selv. Disse journalføres ikke. Merk at brukere kan få
-        innsyn i interne notater hvis de ber om det.
+        Her kan du legge inn kommentarer til deg selv eller andre som skal behandle oppgaven. Kommentarer vises både på
+        oppgaven her i Hotsak og i Gosys uavhengig av hvor kommentaren er skrevet. Vær oppmerksom på at hele eller deler
+        av kommentarer kan være synlig i ulike sammenhenger, både i lister og for ulike enheter og ressurser.
+        Kommentarer kan ikke endres eller slettes når de er lagret. Merk at brukere kan få innsyn i kommentarer hvis de
+        ber om det.
       </BodyLong>
       <Label size="small" as="div">
-        Forvaltningsnotat
+        Forvaltningsnotater
       </Label>
       <BodyLong size="small">
-        Brukes hvis du skal dokumentere opplysninger som kan ha betydning for utfallet av en sak. Disse notatene blir
-        journalført. Vi har to typer forvaltningsnotat:
+        Her kan du dokumentere opplysninger som kan ha betydning for utfallet av en sak. Forvaltningsnotater kan ikke
+        endres eller slettes når de er journalført. Vi har to typer forvaltningsnotater:
       </BodyLong>
       <Box marginBlock="space-12" asChild>
         <List data-aksel-migrated-v8 size="small" as="ul">
@@ -25,8 +28,8 @@ export function Notatinformasjon() {
             </Label>
             <BodyLong size="small">
               Opplysninger som kan ha betydning for saken som for eksempel gjengir innholdet i en iakttakelse, et møte,
-              en befaring eller en uttalelse fra intern fagperson. Notatet vil ikke være synlig på brukers side på
-              nav.no, men bruker vil kunne be om innsyn i det.
+              en befaring eller en uttalelse fra intern fagperson. Forvaltningsnotatet vil ikke være synlig på brukers
+              side på nav.no, men bruker vil kunne be om innsyn i det.
             </BodyLong>
           </List.Item>
           <List.Item>
@@ -34,8 +37,8 @@ export function Notatinformasjon() {
               Eksterne saksopplysninger:
             </Label>
             <BodyLong size="small">
-              Opplysninger som gjengir innholdet i en henvendelse eller dialog med tredjepart. Bruker vil få innsyn i
-              notatet på innlogget side på nav.no fra første virkedag etter at det er ferdigstilt.
+              Opplysninger som gjengir innholdet i en henvendelse eller dialog med tredjepart. Bruker har innsyn i
+              forvaltningsnotatet på innlogget side på nav.no fra første virkedag etter at det er ferdigstilt.
             </BodyLong>
           </List.Item>
         </List>

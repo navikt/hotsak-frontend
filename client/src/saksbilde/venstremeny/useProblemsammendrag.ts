4cb5fc311ece4bdcf58a06ab02c44207c1d98c36
murvold <mads.severin.murvold@nav.no>
2026-06-29 10:54:56 +0200

    feat: bruker postbegrunnelser fra backend

--- client/src/saksbilde/venstremeny/useProblemsammendrag.ts
+++ client/src/saksbilde/venstremeny/useProblemsammendrag.ts
@@ -3,23 +3,26 @@ import useSwr from 'swr'
 import { HttpError } from '../../io/HttpError.ts'
 import { useSakId } from '../useSak.ts'
 
-interface UseProblemsammendragResponse {
+interface UseServiceforespørselResponse {
   sammendragMedLavere: boolean
   problemsammendrag: string
+  postbegrunnelser: string[]
 }
 
-interface ProblemsammendragResponse {
+interface ServiceforespørselResponse {
   problemsammendrag: string
+  postbegrunnelser: string[]
 }
 
-export function useProblemsammendrag(): UseProblemsammendragResponse {
+export function useServiceforespørsel(): UseServiceforespørselResponse {
   const sakId = useSakId()
-  const { data, ...rest } = useSwr<ProblemsammendragResponse, HttpError>(
+  const { data, ...rest } = useSwr<ServiceforespørselResponse, HttpError>(
     sakId ? `/api/sak/${sakId}/serviceforesporsel` : null
   )
   return {
     sammendragMedLavere: data?.problemsammendrag?.startsWith('POST ') ?? false,
     problemsammendrag: data?.problemsammendrag ?? '',
+    postbegrunnelser: data?.postbegrunnelser ?? [],
     ...rest,
   }
 }

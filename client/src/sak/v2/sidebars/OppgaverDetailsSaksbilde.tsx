e2ad2341d91d5cb951387d7685403be482ee5c18
murvold <mads.severin.murvold@nav.no>
2026-08-26 10:40:52 +0200

    styling: visuelt tweaker dokumentvisning på en oppgave

--- client/src/sak/v2/sidebars/OppgaverDetailsSaksbilde.tsx
+++ client/src/sak/v2/sidebars/OppgaverDetailsSaksbilde.tsx
@@ -1,4 +1,4 @@
-import { BodyShort, Link, Tag, VStack } from '@navikt/ds-react'
+import { BodyShort, HStack, Link, Tag, VStack } from '@navikt/ds-react'
 import { DataGridContentProps } from '../../../felleskomponenter/data/DataGrid'
 import { Oppgave } from '../../../oppgave/oppgaveTypes'
 import { useAlleJournalposterForSak } from '../../../saksbilde/useJournalposter'
@@ -13,13 +13,15 @@ export function OppgaveDetailsSaksbilde({ row: oppgave }: DataGridContentProps<O
         <strong>Dokumenter i saken:</strong>
         <ul>
           {dokumenter.map((dokument) => (
-            <li key={dokument.dokumentId}>
-              <Link href={`/api/journalpost/${dokument.journalpostId}/${dokument.dokumentId}`} target="_blank">
-                {dokument.tittel}
-              </Link>{' '}
-              <Tag size="small" variant="outline">
-                {storForbokstavIOrd(dokument.type)}
-              </Tag>
+            <li key={dokument.dokumentId} style={{ marginBottom: '0.2rem' }}>
+              <HStack justify="space-between" width="50%">
+                <Link href={`/api/journalpost/${dokument.journalpostId}/${dokument.dokumentId}`} target="_blank">
+                  {dokument.tittel}
+                </Link>
+                <Tag size="small" variant="outline">
+                  {storForbokstavIOrd(dokument.type)}
+                </Tag>
+              </HStack>
             </li>
           ))}
         </ul>

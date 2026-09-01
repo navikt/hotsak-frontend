f98ad87e0709429c7104a1b4446ec11d4b225504
Erik Eiesland <erik.eiesland@nav.no>
2026-09-01 10:22:12 +0200

    feat: felles Outlet for oppgavelistene, også index-route
    https://trello.com/c/UPy1j9xV

--- client/src/AppRoutes.tsx
+++ client/src/AppRoutes.tsx
@@ -29,12 +29,14 @@ export function AppRoutes() {
 
           {/* Ruter hvor det er naturlig med global, vertikal scroll */}
           <Route element={<SideLayout />}>
-            <Route index element={<MineOppgaver />} />
-            <Route path="oppgaver" element={<Oppgaver />}>
+            <Route element={<Oppgaver />}>
               <Route index element={<MineOppgaver />} />
-              <Route path="mine" element={<MineOppgaver />} />
-              <Route path="enhetens" element={<EnhetensOppgaver />} />
-              <Route path="medarbeiders" element={<MedarbeidersOppgaver />} />
+              <Route path="oppgaver">
+                <Route index element={<MineOppgaver />} />
+                <Route path="mine" element={<MineOppgaver />} />
+                <Route path="enhetens" element={<EnhetensOppgaver />} />
+                <Route path="medarbeiders" element={<MedarbeidersOppgaver />} />
+              </Route>
             </Route>
             <Route path="personoversikt/*" element={<Personoversikt />} />
           </Route>

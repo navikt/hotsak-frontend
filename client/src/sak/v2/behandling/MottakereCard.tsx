c053b7f16bfa19ba580c3fb55673088f3c5ac45e
murvold <mads.severin.murvold@nav.no>
2026-09-23 14:54:57 +0200

    feat: viser kun mottakere hvis verge

--- client/src/sak/v2/behandling/MottakereCard.tsx
+++ client/src/sak/v2/behandling/MottakereCard.tsx
@@ -12,44 +12,53 @@ import { formaterNavn, storForbokstavIOrd } from '../../../utils/formater.ts'
 export function MottakereCard({ vedtaksbrevId }: { vedtaksbrevId?: string }) {
   const { data, error } = useBrevmottakere(vedtaksbrevId)
   const { brev } = useBrev(vedtaksbrevId)
+  const { sak } = useSak()
+
+  const { personInfo } = usePerson(sak?.data.innsender.fnr)
+  const vergemål = personInfo?.vergemål || []
+  const harVerge = vergemål.length > 0
 
   if (!data || error || !brev) return null
 
   return (
     <Box>
-      <Box marginBlock="space-0 space-8">
-        <Heading level="2" size="xsmall">
-          Hvem mottar dette brevet?
-        </Heading>
-      </Box>
-      <CompactExpandableCard variant="subtle" tittel="Mottakere" defaultOpen={false}>
-        <Table size="small">
-          <Table.Body>
-            {data.brevmottakere.map((mottaker) => {
-              if (mottaker.mottakertype == Mottakertype.FORMIDLER) return null //special case
-              const mottakertype = storForbokstavIOrd(mottaker.mottakertype)
-              return (
-                <Table.Row key={mottaker.id}>
-                  <Table.DataCell scope="row">
-                    <Tekst>{mottakertype}</Tekst>
-                  </Table.DataCell>
-                  <Table.DataCell align="right">
-                    {mottaker.kanSlettes ? (
-                      <Tooltip content={`${mottakertype} kan fjernes som mottaker av brevet`}>
-                        <PadlockUnlockedIcon fontSize="1.5rem" />
-                      </Tooltip>
-                    ) : (
-                      <Tooltip content={`${mottakertype} er låst som mottaker av brevet`}>
-                        <PadlockLockedIcon fontSize="1.5rem" />
-                      </Tooltip>
-                    )}
-                  </Table.DataCell>
-                </Table.Row>
-              )
-            })}
-          </Table.Body>
-        </Table>
-      </CompactExpandableCard>
+      {harVerge && (
+        <>
+          <Box marginBlock="space-0 space-8">
+            <Heading level="2" size="xsmall">
+              Hvem mottar dette brevet?
+            </Heading>
+          </Box>
+          <CompactExpandableCard variant="subtle" tittel="Mottakere" defaultOpen={false}>
+            <Table size="small">
+              <Table.Body>
+                {data.brevmottakere.map((mottaker) => {
+                  if (mottaker.mottakertype == Mottakertype.FORMIDLER) return null //special case
+                  const mottakertype = storForbokstavIOrd(mottaker.mottakertype)
+                  return (
+                    <Table.Row key={mottaker.id}>
+                      <Table.DataCell scope="row">
+                        <Tekst>{mottakertype}</Tekst>
+                      </Table.DataCell>
+                      <Table.DataCell align="right">
+                        {mottaker.kanSlettes ? (
+                          <Tooltip content={`${mottakertype} kan fjernes som mottaker av brevet`}>
+                            <PadlockUnlockedIcon fontSize="1.5rem" />
+                          </Tooltip>
+                        ) : (
+                          <Tooltip content={`${mottakertype} er låst som mottaker av brevet`}>
+                            <PadlockLockedIcon fontSize="1.5rem" />
+                          </Tooltip>
+                        )}
+                      </Table.DataCell>
+                    </Table.Row>
+                  )
+                })}
+              </Table.Body>
+            </Table>
+          </CompactExpandableCard>
+        </>
+      )}
       <DelKopiMedFormidler brevId={vedtaksbrevId} mottakere={data} />
     </Box>
   )
@@ -81,7 +90,7 @@ function DelKopiMedFormidler({ brevId, mottakere }: { brevId?: string; mottakere
     <Box marginBlock="space-12 space-8">
       <HStack gap="space-2">
         <Heading size="xsmall" level="2" spacing={false}>
-          Del kopi med formidler
+          Del digital kopi med formidler
         </Heading>
         <HelpText title="Vurder å dele kopi med formidler">
           Hvis du deler en kopi av brevet med formidler, vil det bli synlig på formidler sine sider i 4 uker fra
